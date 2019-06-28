cf36c2970f5177cf0eedd23a9df7b0f87fed3aeb
Yiming Yang <yang.yihming@gmail.com>
2019-06-28 13:46:01 -0400

    BBKNN on hvg subset

--- batch_correction/bbknn/scanpy_bbknn.py
+++ batch_correction/bbknn/scanpy_bbknn.py
@@ -5,11 +5,23 @@ import scanpy as sc
 from bbknn import bbknn
 
 src_file = "../MantonBM_nonmix_tiny_10x_filter_norm_pca.h5ad"
+hvg_file = "../hvg.txt"
 
 print("Reading ICA (bone marrow) dataset")
 adata = sc.read_h5ad(src_file)
 
-print("Computing neighborhood graph using BBKNN...")
-bbknn(adata, batch_key = 'Channel', metric = 'euclidean')
+print("Getting Highly Variable Genes from scCloud")
+df_hvg = pd.read_csv(hvg_file)
+print("Highly variable gene set of size " + str(df_hvg.shape[0]))
+df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
+df_hvg.set_index('index', inplace = True)
+df_hvg.drop(columns = ['gene_ids'], inplace = True)
+adata.var.drop(columns = ['highly_variable_genes'], inplace = True)
+df_join = adata.var.join(df_hvg)
+df_join['highly_variable_genes'].fillna(False, inplace = True)
+adata_variable_genes = adata[:, df_join['highly_variable_genes']].copy()
 
-adata.write("scanpy_bbknn_corrected.h5ad")
+print("Computing neighborhood graph using BBKNN...")
+bbknn(adata_variable_genes, batch_key = 'Channel', metric = 'euclidean')
+
+adata_variable_genes.write("scanpy_bbknn_corrected.h5ad")

a050085ae45968494ed65fa8ed517a6f3c405513
Yiming Yang <yang.yihming@gmail.com>
2019-06-30 04:49:15 -0400

    add de_analysis and annotate_cluster in sccloud correction python file

--- batch_correction/scCloud/run_sccloud_batch_correct.py
+++ batch_correction/scCloud/run_sccloud_batch_correct.py
@@ -5,3 +5,9 @@ print("Use {} cores.".format(n_jobs))
 
 if os.system("scCloud cluster -p {cores} --correct-batch-effect --run-leiden --run-approximated-leiden --run-fitsne --run-umap ../../MantonBM_nonmix_tiny_10x.h5 tiny_sccloud_corrected".format(cores = n_jobs)):
 	sys.exit(1)
+
+if os.system("scCloud de_analysis -p {cores} --labels leiden_labels tiny_sccloud_corrected.h5ad tiny_sccloud_corrected.de.xlsx".format(cores = n_jobs)):
+	sys.exit(1)
+
+if os.system("scCloud annotate_cluster tiny_sccloud_corrected.h5ad tiny_sccloud_corrected.anno.txt"):
+	sys.exit(1)

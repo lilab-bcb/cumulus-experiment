59d74a93ef2496411a5022671fbb515a49d35f67
Yiming Yang <yang.yihming@gmail.com>
2019-06-30 21:30:52 -0400

    use approx_leiden_labels for classifying cluster 10

--- batch_correction/ground/gen_celltype.py
+++ batch_correction/ground/gen_celltype.py
@@ -8,15 +8,15 @@ data_location = "ground.h5ad"
 
 adata = scCloud.tools.read_input(data_location, mode = 'a')
 
-idx = np.isin(adata.obs['leiden_labels'], ['1', '4', '5', '6', '8'])
-X_train, X_test, y_train, y_test = train_test_split(adata.X[idx], adata.obs.loc[idx, 'leiden_labels'], test_size = 0.1, random_state = 0, stratify = adata.obs.loc[idx, 'leiden_labels'])
+idx = np.isin(adata.obs['approx_leiden_labels'], ['1', '4', '5', '6', '8'])
+X_train, X_test, y_train, y_test = train_test_split(adata.X[idx], adata.obs.loc[idx, 'approx_leiden_labels'], test_size = 0.1, random_state = 0, stratify = adata.obs.loc[idx, 'approx_leiden_labels'])
 lgb = LGBMClassifier(n_jobs = 8, metric = 'multi_error', importance_type = 'gain')
 lgb.fit(X_train, y_train, eval_set = [(X_train, y_train), (X_test, y_test)], early_stopping_rounds = 1)
 
-idx10 = np.isin(adata.obs['leiden_labels'], '10')
+idx10 = np.isin(adata.obs['approx_leiden_labels'], '10')
 res = lgb.predict(adata.X[idx10])
 
-adata.obs['cell_types'] = adata.obs['leiden_labels'].astype('object')
+adata.obs['cell_types'] = adata.obs['approx_leiden_labels'].astype('object')
 adata.obs.loc[idx10, 'cell_types'] = res
 adata.obs['cell_types'] = pd.Categorical(adata.obs['cell_types'], categories = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '15', '16', '17', '18', '19'])
 adata.obs['cell_types'] = adata.obs['cell_types'].apply(lambda s: '3' if s == '13' else s)

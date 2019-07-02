f82c46e756104814444bd88dfa7121977c114ee7
Yiming Yang <yang.yihming@gmail.com>
2019-07-02 00:00:52 -0400

    only do PCA and UMAP computation in scCloud correction

--- batch_correction/scCloud/run_sccloud_batch_correct.py
+++ batch_correction/scCloud/run_sccloud_batch_correct.py
@@ -3,11 +3,5 @@ import os, sys
 n_jobs = os.cpu_count()
 print("Use {} cores.".format(n_jobs))
 
-if os.system("scCloud cluster -p {cores} --correct-batch-effect --run-approximated-leiden --run-fitsne --run-umap ../../MantonBM_nonmix_tiny_10x.h5 tiny_sccloud_corrected".format(cores = n_jobs)):
-	sys.exit(1)
-
-if os.system("scCloud de_analysis -p {cores} --labels approx_leiden_labels tiny_sccloud_corrected.h5ad tiny_sccloud_corrected.de.xlsx".format(cores = n_jobs)):
-	sys.exit(1)
-
-if os.system("scCloud annotate_cluster tiny_sccloud_corrected.h5ad tiny_sccloud_corrected.anno.txt"):
+if os.system("scCloud cluster -p {cores} --correct-batch-effect --run-umap ../../MantonBM_nonmix_tiny_10x.h5 tiny_sccloud_corrected".format(cores = n_jobs)):
 	sys.exit(1)

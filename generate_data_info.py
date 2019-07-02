f418c3a44d0471fa5bbc6c70ae96a7c7602a6a14
Yiming Yang <yang.yihming@gmail.com>
2019-07-02 16:07:22 -0400

    fix bugs

--- generate_data_info.py
+++ generate_data_info.py
@@ -27,18 +27,13 @@ def preprocess_data(in_file):
 
 	# Filtration and Normalization.
 	suffix1 = "_filter_norm"
-	output_name1 = in_file + suffix1
+	output_name1 = in_file.split('.')[0] + suffix1
 
 	adata = scCloud.tools.read_input(in_file)
 	scCloud.tools.update_var_names(adata)
 	scCloud.tools.filter_data(adata)
 	scCloud.tools.log_norm(adata, norm_count = 1e5)
 	
-	scCloud.tools.write_output(adata, output_name1)
-	
-	# Furthermore, do PCA.
-	suffix2 = "_filter_norm_pca"
-	output_name2 = in_file + suffix2
 	print("Getting Highly Variable Gene Set...")
 	df_hvg = pd.read_csv(hvg_file)
 	df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
@@ -47,11 +42,18 @@ def preprocess_data(in_file):
 	adata.var.drop(columns = ['highly_variable_genes'], inplace = True)
 	df_join = adata.var.join(df_hvg)
 	df_join['highly_variable_genes'].fillna(False, inplace = True)
-	adata_variable_genes = adata[:, df_join['highly_variable_genes']].copy()
-	scCloud.tools.run_pca(adata_variable_genes)
+	adata_hvg = adata[:, df_join['highly_variable_genes']].copy()
 
-	adata.obsm['X_pca'] = adata_variable_genes.obsm['X_pca']
-	scCloud.tools.write_output(adata, output_name2)
+	scCloud.tools.write_output(adata_hvg, output_name1)
+
+	# Furthermore, do PCA.
+	suffix2 = "_filter_norm_pca"
+	output_name2 = in_file.split('.')[0] + suffix2
+	adata_hvg_c = adata_hvg.copy()
+	scCloud.tools.run_pca(adata_hvg_c)
+
+	adata_hvg.obsm['X_pca'] = adata_hvg_c.obsm['X_pca']
+	scCloud.tools.write_output(adata_hvg, output_name2)
 
 if __name__ == '__main__':
 	extract_hvg()

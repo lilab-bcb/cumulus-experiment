94f5640419408dc59e90bdabbcdb88f43ee2bcbb
Yiming Yang <yang.yihming@gmail.com>
2019-07-15 14:51:19 -0400

    update overall experiment code

--- overall/run_scanpy.py
+++ overall/run_scanpy.py
@@ -1,97 +1,102 @@
+#!/usr/bin/env python
+
 import numpy as np
-import pandas as pd
-import os, sys, time
 import scanpy as sc
-from bbknn import bbknn
+import os, sys, time
 
 sc.settings.n_jobs = os.cpu_count()
 rand_seed = 0
-filter_norm_data = "../MantonBM_nonmix_10x_filter_norm.h5ad"
-hvg_file = "../hvg.txt"
-pca_data = "../MantonBM_nonmix_10x_filter_norm_pca.h5ad"
-corrected_data = "./sccloud_output/MantonBM_nonmix_sccloud_corrected.h5ad"
+output_dir = "scanpy_output"
 
-f = open(output_dir + "scanpy.log", "w")
+if output_dir not in os.listdir('.'):
+	if os.system("mkdir {}".format(output_dir)):
+		sys.exit(1)
+
+f = open(output_dir + "/scanpy.log", "w")
 
 print("Reading ICA (bone marrow) dataset")
-start = time.time()
-adata_filter_norm = sc.read_h5ad(filter_norm_data)
-print("Finding variable genes")
+adata = sc.read_h5ad(filter_norm_data)
+print("Finding highly variable genes")
 start_hvg = time.time()
 sc.pp.highly_variable_genes(adata_filter_norm, min_mean=0.0125, max_mean=7, min_disp=0.5)
 end_hvg = time.time()
 print("Time spent for HVG = {:.2f} seconds.".format(end_hvg - start_hvg))
 f.write("Time spent for HVG = {:.2f} seconds.\n".format(end_hvg - start_hvg))
 
-print("Getting Highly Variable Genes from scCloud")
+print("Batch correction using BBKNN")
 df_hvg = pd.read_csv(hvg_file)
-print("Highly variable gene set of size " + str(df_hvg.shape[0]))
 df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
 df_hvg.set_index('index', inplace = True)
 df_hvg.drop(columns = ['gene_ids'], inplace = True)
-adata_filter_norm.var.drop(columns = ['highly_variable_genes'], inplace = True)
-df_join = adata_filter_norm.var.join(df_hvg)
+adata = sc.read_h5ad(pca_data)
+df_join = adata.var.join(df_hvg)
 df_join['highly_variable_genes'].fillna(False, inplace = True)
-adata_variable_genes = adata_filter_norm[:, df_join['highly_variable_genes']].copy()
-
-
-print("Scale expression matrix of variable genes")
-sc.pp.scale(adata_variable_genes, max_value=10)
+adata_hvg = adata[:, df_join['highly_variable_genes']].copy()
+start_bbknn = time.time()
+bbknn(adata_hvg, batch_key = 'Channel', metric = 'euclidean')
+end_bbknn = time.time()
+print("Time spend for BBKNN batch correction = {:.2f} seconds.".format(end_bbknn - start_bbknn))
+f.write("Time spend for BBKNN batch correction = {:.2f} seconds.\n".format(end_bbknn - start_bbknn))
 
 print("Running PCA")
+adata = sc.read_h5ad(corrected_data)
+adata_hvg = adata[:, adata.var['highly_variable_genes']].copy()
 start_pca = time.time()
-sc.tl.pca(adata_variable_genes, random_state = rand_seed)
+sc.pp.scale(adata_hvg, max_value = 10)
+sc.tl.pca(adata_hvg, random_state = rand_seed)
 end_pca = time.time()
-adata.obsm = adata_variable_genes.obsm
-print("Time spent for PCA = " + str(end_pca - start_pca) + " seconds.")
-f.write("Time spent for PCA = " + str(end_pca - start_pca) + " seconds.\n")
+print("Time spent for PCA = {:.2f} seconds.".format(end_pca - start_pca))
+f.write("Time spent for PCA = {:.2f} seconds.\n".format(end_pca - start_pca))
+
+
 
 print("Computing neighborhood graph")
-start_bbknn = time.time()
-bbknn(adata, batch_key = 'Channel', metric = 'euclidean')
-end_bbknn = time.time()
-print("Time spent for bbknn = " + str(end_nn - start_nn) + " seconds.")
-f.write("Time spent for bbknn = " + str(end_nn - start_nn) + " seconds.\n")
+adata = sc.read_h5ad(corrected_data)
+start_nn = time.time()
+sc.pp.neighbors(adata, n_neighbors = 100, random_state = rand_seed)
+end_nn = time.time()
+print("Time spent for knn = {:.2f} seconds.".format(end_nn - start_nn))
+f.write("Time spent for knn = {:.2f} seconds.\n".format(end_nn - start_nn))
+
 
 print("Finding Clusters")
 start_cluster = time.time()
 sc.tl.leiden(adata, resolution = 1.3, random_state = rand_seed)
 end_cluster = time.time()
-print("Time spent for leiden = " + str(end_lv - start_lv) + " seconds.")
-f.write("Time spent for leiden = " + str(end_lv - start_lv) + " seconds.\n")
+print("Time spent for leiden = {:.2f} seconds.".format(end_cluster - start_cluster))
+f.write("Time spent for leiden = {:.2f} seconds.\n".format(end_cluster - start_cluster))
 
 print("Computing UMAP embedding")
-start_up = time.time()
+start_umap = time.time()
 sc.tl.umap(adata, random_state = rand_seed)
-end_up = time.time()
-print("Time spent for UMAP = " + str(end_up - start_up) + " seconds.")
-f.write("Time spent for UMAP = " + str(end_up - start_up) + "seconds.\n")
+end_umap = time.time()
+print("Time spent for UMAP = {:.2f} seconds.".format(end_umap - start_umap))
+f.write("Time spent for UMAP = {:.2f} seconds.\n".format(end_umap - start_umap))
 
 print("Computing tSNE embedding")
-start_te = time.time()
-sc.tl.tsne(adata, n_pcs = 2, random_state = rand_seed, use_fast_tsne = True, n_jobs = sc.settings.n_jobs)
-end_te = time.time()
-print("Time spent for tSNE = " + str(end_te - start_te) + " seconds.")
-f.write("Time spent for tSNE = " + str(end_te - start_te) + " seconds.\n")
+start_tsne = time.time()
+sc.tl.tsne(adata, n_pcs = 2, use_rep = 'X_pca', random_state = rand_seed, use_fast_tsne = True)
+end_tsne = time.time()
+print("Time spent for tSNE = {:.2f} seconds.".format(end_tsne - start_tsne))
+f.write("Time spent for tSNE = {:.2f} seconds.\n".format(end_tsne - start_tsne))
 
 print("Computing FLE embedding")
 start_fle = time.time()
 sc.tl.draw_graph(adata, random_state = rand_seed, n_jobs = sc.settings.n_jobs)
 end_fle = time.time()
-print("Time spent for FLE = " + str(end_fle - start_fle) + " seconds.")
-f.write("Time spent for FLE = " + str(end_fle - start_fle) + " seconds.\n")
+print("Time spent for FLE = {:.2f} seconds.".format(end_fle - start_fle))
+f.write("Time spent for FLE = {:.2f} seconds.\n".format(end_fle - start_fle))
 
 print("Computing diffmap")
+adata = sc.read_h5ad(corrected_data)
+sc.pp.neighbors(adata, n_neighbors = 100, random_state = rand_seed, method = 'gauss')
 start_df = time.time()
 sc.tl.diffmap(adata, n_comps = 50)
 end_df = time.time()
-print("Time spent for diffmap = " + str(end_df - start_df) + " seconds.")
-f.write("Time spent for diffmap = " + str(end_df - start_df) + " seconds.\n")
+print("Time spent for diffmap = {:.2f} seconds.".format(end_df - start_df))
+f.write("Time spent for diffmap = {:.2f} seconds.\n".format(end_df - start_df))
 
-adata.write(output_dir + "/MantonBM_nonmix_scanpy.h5ad")
-end = time.time()
-print("Total time = " + str(end - start) + " seconds.")
-f.write("Total time = " + str(end - start) + " seconds.")
+
 f.close()
 
 #sc.pl.tsne(adata, color = ['leiden'], save = '.pdf')
--- overall/run_seurat3.R
+++ overall/run_seurat3.R
@@ -7,84 +7,48 @@ n.cores <- detectCores()
 setOption('mc.cores', n.cores)
 print(paste("Use", n.cores, "cores."))
 
-<<<<<<< HEAD
-start <- Sys.time()
-adata <- Read10X_h5("../MantonBM_nonmix_10x.h5")
-rownames(adata) <- make.names(adata[,1], unique = TRUE)
-ica <- CreateSeuratObject(counts = adata, project = "ICA", min.cells = 138)
-print("Read and Initialization:")
-
-now <- Sys.time()
-mito.features <- grep(pattern = "^MT-", rownames(ica), value = TRUE)
-percent.mito <- Matrix::colSums(GetAssayData(ica, slot = 'counts')[mito.features,]) / Matrix::colSums(GetAssayData(ica, slot = 'counts'))
-ica <- subset(ica, subset = nFeature_RNA >=500 & nFeature_RNA < 6000 & percent.mito < 0.1)
-print("Filtering cells:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-ica <- NormalizeData(ica, selection.method = "LogNormalize", scale.factor = 1e5)
-print("Normalizing data:")
-print(Sys.time() - now)
-
-=======
 
 ## Benchmark Highly Variable Gene selection.
 ica <- ReadH5AD("../MantonBM_nonmix_10x_filter_norm.h5ad")
->>>>>>> 2f87f20... don't upload tar.gz
 now <- Sys.time()
 ica <- FindVariableFeatures(ica, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
 print("Finding Highly Variable Genes:")
 print(Sys.time() - now)
 
 ## Benchmark PCA.
-adata <- ReadH5AD("./sccloud_output/MantonBM_nonmix_sccloud_corrected.h5ad")
-now <- Sys.time()
-##ica <- ScaleData(ica, features = rownames(ica), scale.max = 10)
-ica <- ScaleData(ica, scale.max = 10)
-print("Scale expression matrix of variable genes:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-print("Running PCA:")
-ica <- RunPCA(ica, features = VariableFeatures(ica), verbose = FALSE, seed.use = seed)
-print("PCA time:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-print("Computing neighborhood graph:")
-ica <- FindNeighbors(ica, k.param = 100, compute.SNN = FALSE) # Issue: Do I have to compute SNN?
-print("kNN time:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-print("Finding Clusters using Leiden:")
-ica <- FindClusters(ica, resolution = 1.3, random.seed = seed, algorithm = 4, verbose = FALSE) # TODO: Check if 2 is faster than 4.
-print("Leiden Clustering time:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-print("Computing UMAP embedding:")
-ica <- RunUMAP(ica, min.dist = 0.5, seed.use = seed, features = ica[["pca"]], verbose = FALSE)
-print("UMAP time:")
-print(Sys.time() - now)
-
-now <- Sys.time()
-print("Computing FItSNE embedding:")
-ica <- RunTSNE(ica, seed.use = seed, tsne.method = "FIt-SNE", features = ica[["pca"]])
-print("FItSNE time:")
-print(Sys.time() - now)
-
-#now <- Sys.time()
-#ica.markers <- FindAllMarkers(ica, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-#print("Finding Markers:")
-#print(Sys.time() - now)
-
-end <- Sys.time()
-print("Total time without writing:")
-print(end - start)
-
-#write.csv(ica.markers, file = "/home/de_analysis_seurat3.csv")
-save(ica, file = "./seurat_output/seurat_result.RData")
-end <- Sys.time()
-print("Total time after R writing:")
-print(end - start)
+##adata <- ReadH5AD("./sccloud_output/MantonBM_nonmix_sccloud_corrected.h5ad")
+##now <- Sys.time()
+####ica <- ScaleData(ica, features = rownames(ica), scale.max = 10)
+##ica <- ScaleData(ica, scale.max = 10)
+##print("Scale expression matrix of variable genes:")
+##print(Sys.time() - now)
+##
+##now <- Sys.time()
+##print("Running PCA:")
+##ica <- RunPCA(ica, features = VariableFeatures(ica), verbose = FALSE, seed.use = seed)
+##print("PCA time:")
+##print(Sys.time() - now)
+##
+##now <- Sys.time()
+##print("Computing neighborhood graph:")
+##ica <- FindNeighbors(ica, k.param = 100, compute.SNN = FALSE) # Issue: Do I have to compute SNN?
+##print("kNN time:")
+##print(Sys.time() - now)
+##
+##now <- Sys.time()
+##print("Finding Clusters using Leiden:")
+##ica <- FindClusters(ica, resolution = 1.3, random.seed = seed, algorithm = 4, verbose = FALSE) # TODO: Check if 2 is faster than 4.
+##print("Leiden Clustering time:")
+##print(Sys.time() - now)
+##
+##now <- Sys.time()
+##print("Computing UMAP embedding:")
+##ica <- RunUMAP(ica, min.dist = 0.5, seed.use = seed, features = ica[["pca"]], verbose = FALSE)
+##print("UMAP time:")
+##print(Sys.time() - now)
+##
+##now <- Sys.time()
+##print("Computing FItSNE embedding:")
+##ica <- RunTSNE(ica, seed.use = seed, tsne.method = "FIt-SNE", features = ica[["pca"]])
+##print("FItSNE time:")
+##print(Sys.time() - now)

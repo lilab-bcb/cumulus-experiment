b71ff28ec377d4abbd55a7cddd1d8d596c86a748
Yiming Yang <yang.yihming@gmail.com>
2019-07-23 12:48:46 -0400

    structure seurat experiment

--- overall/run_scanpy.py
+++ overall/run_scanpy.py
@@ -4,6 +4,8 @@ import pandas as pd
 import scanpy as sc
 from bbknn import bbknn
 from scanpy.neighbors import compute_connectivities_umap
+from datetime import timedelta
+from termcolor import cprint
 
 sc.settings.n_jobs = os.cpu_count()
 sc.settings.verbosity = 4
@@ -16,50 +18,57 @@ corrected_data = "../MantonBM_nonmix_corrected.h5ad"
 
 f = open("scanpy.log", "w")
 
-#print("Reading ICA (bone marrow) dataset")
-#adata = sc.read_h5ad(filter_norm_data)
-#print("Finding highly variable genes")
-#start_hvg = time.time()
-#sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=7, min_disp=0.5, n_top_genes = 2000, batch_key = 'Channel')
-#end_hvg = time.time()
-#print("Time spent for HVG = {:.2f} seconds.".format(end_hvg - start_hvg))
-#f.write("Time spent for HVG = {:.2f} seconds.\n".format(end_hvg - start_hvg))
-
-#print("Batch correction using BBKNN")
-#df_hvg = pd.read_csv(hvg_file)
-#df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
-#df_hvg.set_index('index', inplace = True)
-#df_hvg.drop(columns = ['gene_ids'], inplace = True)
-#adata = sc.read_h5ad(pca_data)
-#df_join = adata.var.join(df_hvg)
-#df_join['highly_variable_genes'].fillna(False, inplace = True)
-#adata_hvg = adata[:, df_join['highly_variable_genes']].copy()
-#start_bbknn = time.time()
-#bbknn(adata_hvg, batch_key = 'Channel')
-#end_bbknn = time.time()
-#print("Time spend for BBKNN batch correction = {:.2f} seconds.".format(end_bbknn - start_bbknn))
-#f.write("Time spend for BBKNN batch correction = {:.2f} seconds.\n".format(end_bbknn - start_bbknn))
-
-#print("Running PCA")
-#adata = sc.read_h5ad(corrected_data)
-#adata_hvg = adata[:, adata.var['highly_variable_genes']].copy()
-#start_pca = time.time()
-#sc.pp.scale(adata_hvg, max_value = 10)
-#sc.tl.pca(adata_hvg, random_state = rand_seed)
-#end_pca = time.time()
-#print("Time spent for PCA = {:.2f} seconds.".format(end_pca - start_pca))
-#f.write("Time spent for PCA = {:.2f} seconds.\n".format(end_pca - start_pca))
-#
-#
-#print("Computing neighborhood graph")
-#adata = sc.read_h5ad(corrected_data)
-#start_nn = time.time()
-#sc.pp.neighbors(adata, n_neighbors = 100, random_state = rand_seed)
-#end_nn = time.time()
-#print("Time spent for knn = {:.2f} seconds.".format(end_nn - start_nn))
-#f.write("Time spent for knn = {:.2f} seconds.\n".format(end_nn - start_nn))
+print("Reading ICA (bone marrow) dataset")
+adata = sc.read_h5ad(filter_norm_data)
+print("Finding highly variable genes")
+start_hvg = time.time()
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=7, min_disp=0.5, n_top_genes = 2000, batch_key = 'Channel')
+end_hvg = time.time()
+logstr_hvg = "Time spent for HVG: {}.".format(timedelta(seconds = end_hvg - start_hvg))
+cprint(logstr_hvg, "yellow")
+f.write(logstr_hvg + '\n')
 
 
+print("Batch correction using BBKNN")
+df_hvg = pd.read_csv(hvg_file)
+df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
+df_hvg.set_index('index', inplace = True)
+df_hvg.drop(columns = ['gene_ids'], inplace = True)
+adata = sc.read_h5ad(pca_data)
+df_join = adata.var.join(df_hvg)
+df_join['highly_variable_genes'].fillna(False, inplace = True)
+adata_hvg = adata[:, df_join['highly_variable_genes']].copy()
+start_bbknn = time.time()
+bbknn(adata_hvg, batch_key = 'Channel')
+end_bbknn = time.time()
+logstr_batch = "Time spend for BBKNN batch correction: {}.".format(timedelta(seconds = end_bbknn - start_bbknn))
+cprint(logstr_batch, "yellow")
+f.write(logstr_batch + '\n')
+
+
+print("Running PCA")
+adata = sc.read_h5ad(corrected_data)
+adata_hvg = adata[:, adata.var['highly_variable_genes']].copy()
+start_pca = time.time()
+sc.pp.scale(adata_hvg, max_value = 10)
+sc.tl.pca(adata_hvg, random_state = rand_seed)
+end_pca = time.time()
+logstr_pca = "Time spent for PCA: {}.".format(timedelta(seconds = end_pca - start_pca))
+cprint(logstr_pca, "yellow")
+f.write(logstr_pca + '\n')
+
+
+print("Computing neighborhood graph")
+adata = sc.read_h5ad(corrected_data)
+start_nn = time.time()
+sc.pp.neighbors(adata, n_neighbors = 100, random_state = rand_seed)
+end_nn = time.time()
+logstr_nn = "Time spent for knn: {}.".format(timedelta(seconds = end_nn - start_nn))
+cprint(logstr_nn, "yellow")
+f.write(logstr_nn + '\n')
+
+
+# Construct affinity matrix using KNN information from scCloud results.
 adata = sc.read_h5ad(corrected_data)
 n_cells = adata.shape[0]
 knn_indices = np.concatenate((np.arange(n_cells).reshape(-1, 1), adata.uns['knn_indices']), axis = 1)
@@ -76,42 +85,41 @@ print("Finding Clusters")
 start_cluster = time.time()
 sc.tl.leiden(adata, resolution = 1.3, random_state = rand_seed)
 end_cluster = time.time()
-print("Time spent for leiden = {:.2f} seconds.".format(end_cluster - start_cluster))
-f.write("Time spent for leiden = {:.2f} seconds.\n".format(end_cluster - start_cluster))
+logstr_cluster = "Time spent for leiden: {}.".format(timedelta(seconds = end_cluster - start_cluster))
+cprint(logstr_cluster, "yellow")
+f.write(logstr_cluster + '\n')
+
 
 print("Computing UMAP embedding")
 start_umap = time.time()
 sc.tl.umap(adata, random_state = rand_seed)
 end_umap = time.time()
-print("Time spent for UMAP = {:.2f} seconds.".format(end_umap - start_umap))
-f.write("Time spent for UMAP = {:.2f} seconds.\n".format(end_umap - start_umap))
+logstr_umap = "Time spent for UMAP: {}.".format(timedelta(seconds = end_umap - start_umap))
+cprint(logstr_umap, "yellow")
+f.write(logstr_umap + '\n')
+
 
 print("Computing tSNE embedding")
 start_tsne = time.time()
 sc.tl.tsne(adata, n_pcs = 2, use_rep = 'X_pca', random_state = rand_seed, use_fast_tsne = True)
 end_tsne = time.time()
-print("Time spent for tSNE = {:.2f} seconds.".format(end_tsne - start_tsne))
-f.write("Time spent for tSNE = {:.2f} seconds.\n".format(end_tsne - start_tsne))
+logstr_tsne = "Time spent for tSNE: {}.".format(timedelta(seconds = end_tsne - start_tsne))
+cprint(logstr_tsne, "yellow")
+f.write(logstr_tsne + '\n')
 
-#print("Computing FLE embedding")
-#start_fle = time.time()
-#sc.tl.draw_graph(adata, random_state = rand_seed, n_jobs = sc.settings.n_jobs)
-#end_fle = time.time()
-#print("Time spent for FLE = {:.2f} seconds.".format(end_fle - start_fle))
-#f.write("Time spent for FLE = {:.2f} seconds.\n".format(end_fle - start_fle))
 
-#print("Computing diffmap")
-#adata = sc.read_h5ad(corrected_data)
-#sc.pp.neighbors(adata, n_neighbors = 100, random_state = rand_seed, method = 'gauss')
-#start_df = time.time()
-#sc.tl.diffmap(adata, n_comps = 50)
-#end_df = time.time()
-#print("Time spent for diffmap = {:.2f} seconds.".format(end_df - start_df))
-#f.write("Time spent for diffmap = {:.2f} seconds.\n".format(end_df - start_df))
-
+print("Computing diffmap")
+start_df = time.time()
+sc.tl.diffmap(adata, n_comps = 50)
+end_df = time.time()
+print("Time spent for diffmap: {}.".format(timedelta(seconds = end_df - start_df)))
+f.write("Time spent for diffmap: {}.".format(timedelta(seconds = end_df - start_df)))
 
 f.close()
 
-#sc.pl.tsne(adata, color = ['leiden'], save = '.pdf')
-#sc.pl.umap(adata, color = ['leiden'], save = '.pdf')
-#sc.pl.draw_graph(adata, color = 'leiden', save = '.pdf')
+print("Computing FLE embedding")
+start_fle = time.time()
+sc.tl.draw_graph(adata, random_state = rand_seed, n_jobs = sc.settings.n_jobs)
+end_fle = time.time()
+logstr_fle = "Time spent for FLE: {}.".format(timedelta(seconds = end_fle - start_fle))
+cprint(logstr_fle, "yellow")
--- overall/run_seurat3.R
+++ overall/run_seurat3.R
@@ -4,68 +4,73 @@ library(parallel)
 
 seed <- 0
 n.cores <- detectCores()
+logfile <- "seurat.log"
 setOption('mc.cores', n.cores)
 print(paste("Use", n.cores, "cores."))
 
 ## Benchmark Highly Variable Gene selection. ##
-##src.obj <- ReadH5AD("../MantonBM_nonmix_10x_filter_norm.h5ad")
-##obj.list <- SplitObject(src.obj, split.by = "Channel")
-##now <- Sys.time()
-##hvg <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
-##print("Finding Highly Variable Genes:")
-##print(Sys.time() - now)
+src.obj <- ReadH5AD("../MantonBM_nonmix_10x_filter_norm.h5ad")
+obj.list <- SplitObject(src.obj, split.by = "Channel")
+print("Finding Highly Variable Genes:")
+now <- Sys.time()
+hvg <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+logstr.hvg <- Sys.time() - now
+print(logstr.hvg)
+write(paste("HVG time:", logstr.hvg), file = logfile)
 
 ## Benchmark Batch Correction. ##
-src.obj <- ReadH5AD("../MantonBM_nonmix_10x_filter_norm_hvg.h5ad")
-VariableFeatures(src.obj) <- GetAssayData(src.obj)@Dimnames[[1]]
-obj.list <- SplitObject(src.obj, split.by = "Channel")
-now <- Sys.time()
-bm.anchors <- FindIntegrationAnchors(obj.list)
-bm.combined <- IntegrateData(bm.anchors)
-print("Batch Correction:")
-print(Sys.time() - now)
+##src.obj <- ReadH5AD("../MantonBM_nonmix_10x_filter_norm_hvg.h5ad")
+##VariableFeatures(src.obj) <- GetAssayData(src.obj)@Dimnames[[1]]
+##obj.list <- SplitObject(src.obj, split.by = "Channel")
+##now <- Sys.time()
+##bm.anchors <- FindIntegrationAnchors(obj.list)
+##bm.combined <- IntegrateData(bm.anchors)
+##print("Batch Correction:")
+##print(Sys.time() - now)
 
-
-
-if (1 == 0) {
 
 ## Benchmark PCA.
 adata <- ReadH5AD("../MantonBM_nonmix_corrected_hvg.h5ad")
+print("PCA time:")
 now <- Sys.time()
 adata <- ScaleData(adata, scale.max = 10, verbose = FALSE)
 adata <- RunPCA(adata, verbose = FALSE, seed.use = seed, features = GetAssayData(adata, slot = "data")@Dimnames[[1]])
-print("PCA time:")
-print(Sys.time() - now)
+logstr.pca <- Sys.time() - now
+print(logstr.pca)
+write(paste("PCA time:", logstr.pca), file = logfile, append = TRUE)
+
 
 save(adata, file = "seurat_pca.RData")
 
 print("Computing neighborhood graph:")
 n.pc <- dim(adata[["pca"]])[2]
 now <- Sys.time()
-adata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc)
-print("kNN time in total:")
-print(Sys.time() - now)
+bdata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, compute.SNN = FALSE)
+logstr.knn <- Sys.time() - now
+print(logstr.knn)
+write(paste("KNN time:", logstr.knn), file = logfile, append = TRUE)
 
+adata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, compute.SNN = TRUE)
 save(adata, file = "seurat_knn.RData")
 
 print("Finding Clusters:")
 now <- Sys.time()
 adata <- FindClusters(adata, resolution = 1.3, random.seed = seed, algorithm = "louvain")
-print("Clustering time:")
-print(Sys.time() - now)
-
-save(adata, file = "seurat_cluster.RData")
+logstr.cluster <- Sys.time() - now
+print(logstr.cluster)
+write(paste("Clustering time:", logstr.cluster), file = logfile, append = TRUE)
 
 print("Computing UMAP embedding:")
 now <- Sys.time()
 ica <- RunUMAP(ica, n.neighbors = 15, min.dist = 0.5, seed.use = seed, features = ica[["pca"]], metric = 'euclidean')
-print("UMAP time:")
-print(Sys.time() - now)
+logstr.umap <- Sys.time() - now
+print(logstr.umap)
+write(paste("UMAP time:", logstr.umap), file = logfile, append = TRUE)
+
 
 print("Computing FItSNE embedding:")
 now <- Sys.time()
 ica <- RunTSNE(ica, seed.use = seed, tsne.method = "FIt-SNE", features = ica[["pca"]])
-print("FItSNE time:")
-print(Sys.time() - now)
-
-}
+logstr.tsne <- Sys.time() - now
+print(logstr.tsne)
+write(paste("TSNE time:", logstr.tsne), file = logfile, append = TRUE)

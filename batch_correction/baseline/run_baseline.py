5fcbbf48343ea27f9e128c6ed4c2d4afd3f64b1a
Yiming Yang <yang.yihming@gmail.com>
2019-08-26 14:03:41 -0400

    update baseline in batch correction experiment

--- batch_correction/baseline/run_baseline.py
+++ batch_correction/baseline/run_baseline.py
@@ -3,5 +3,11 @@ import os, sys
 n_jobs = os.cpu_count()
 print("Use {} cores.".format(n_jobs))
 
-if os.system("scCloud cluster -p {cores} --run-umap ../../MantonBM_nonmix_tiny_10x.h5 baseline_result".format(cores = n_jobs)):
+if os.system("sccloud cluster -p {cores} --umap ../../MantonBM_nonmix_tiny.h5sc baseline_result".format(cores = n_jobs)):
+	sys.exit(1)
+
+if os.system("sccloud de_analysis -p 8 --labels spectral_leiden_labels --t baseline_result.h5ad baseline_result.de.xlsx"):
+	sys.exit(1)
+
+if os.system("sccloud annotate_cluster baseline_result.h5ad baseline_result.anno.txt"):
 	sys.exit(1)
--- batch_correction/ground/run_sccloud.sh
+++
@@ -1,3 +0,0 @@
-scCloud cluster -p 8 --run-approximated-leiden --run-fitsne --run-umap ../../MantonBM_nonmix_tiny_10x.h5 ground
-scCloud de_analysis -p 8 --labels approx_leiden_labels ground.h5ad ground.de.xlsx
-scCloud annotate_cluster ground.h5ad ground.anno.txt

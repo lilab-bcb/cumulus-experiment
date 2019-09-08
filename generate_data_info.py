394354f225a1e67c4f02d8ee8c7ad6b972f08fd6
Yiming Yang <yang.yihming@gmail.com>
2019-09-08 16:10:26 -0400

    update data generation

--- generate_data_info.py
+++ generate_data_info.py
@@ -1,41 +1,37 @@
 import os, sys
-import scCloud
+import sccloud as scc
 import pandas as pd
 from termcolor import cprint
 
-full_data_source = "MantonBM_nonmix_10x.h5"
-tiny_data_source = "MantonBM_nonmix_tiny_10x.h5"
+full_data_source = "MantonBM_nonmix.h5sc"
+tiny_data_source = "MantonBM_nonmix_tiny.h5sc"
 
-hvg_file = "hvg.txt"
+hvf_file = "hvf.txt"
 
-def extract_hvg():
+def extract_hvf():
 
 	# Run correction.
 	cprint("Running scCloud batch correction on full data...", "green")
-	adata = scCloud.tools.read_input(full_data_source)
-	scCloud.tools.update_var_names(adata)
-	scCloud.tools.filter_data(adata)
-	scCloud.tools.log_norm(adata, norm_count = 1e5)
-	scCloud.tools.set_group_attribute(adata, attribute_string = None)
-	scCloud.tools.estimate_adjustment_matrices(adata)
-	scCloud.tools.select_highly_variable_genes(adata, consider_batch = True, n_jobs = 8)
+	adata = scc.read_input(full_data_source)
+	scc.qc_metrics(adata)
+	scc.filter_data(adata)
+	scc.log_norm(adata)
+	scc.highly_variable_features(adata, consider_batch = True, n_jobs = 8)
+	scc.correct_batch(adata, features = "highly_variable_features")
 
-	adata_c = scCloud.tools.collect_highly_variable_gene_matrix(adata)
-	scCloud.tools.correct_batch_effects(adata_c)
-	scCloud.tools.run_pca(adata_c)
-	adata.obsm['X_pca'] = adata_c.obsm['X_pca']
-	
-	scCloud.tools.get_kNN(adata, 'X_pca', K = 100, n_jobs = 8)
+	# PCA and KNN
+	scc.pca(adata)
+	scc.neighbors(adata, n_jobs = 8)
 
 	# Extract highly variable gene set to file.
-	cprint("Extracting highly variable gene set to file...", "green")
-	df_hvg = adata.var.loc[adata.var["highly_variable_genes"]][["gene_ids"]]
-	df_hvg.to_csv(hvg_file)
+	cprint("Extracting highly variable features to file...", "green")
+	df_hvf = adata.var.loc[adata.var["highly_variable_features"]][["gene_ids"]]
+	df_hvf.to_csv(hvf_file, index_label = "index")
 
-	scCloud.tools.write_output(adata, "MantonBM_nonmix_corrected")
+	scc.write_output(adata, "MantonBM_nonmix_corrected")
 
-	bdata = adata[:, adata.var['highly_variable_genes']].copy()
-	scCloud.tools.write_output(bdata, "MantonBM_nonmix_corrected_hvg")
+	bdata = adata[:, adata.var['highly_variable_features']].copy()
+	scc.write_output(bdata, "MantonBM_nonmix_corrected_hvg")
 
 def preprocess_data(in_file):
 
@@ -43,41 +39,39 @@ def preprocess_data(in_file):
 
 	# Filtration and Normalization.
 	suffix1 = "_filter_norm"
-	output_name1 = in_file.split('.')[0] + suffix1
-	output_hvg_name1 = output_name1 + "_hvg"
+	output_name1 = os.path.splitext(in_file)[0] + suffix1
+	output_hvf_name1 = output_name1 + "_hvf"
 
-	adata = scCloud.tools.read_input(in_file)
-	scCloud.tools.update_var_names(adata)
-	scCloud.tools.filter_data(adata)
-	scCloud.tools.log_norm(adata, norm_count = 1e5)
-	
-	print("Getting Highly Variable Gene Set...")
-	df_hvg = pd.read_csv(hvg_file)
-	df_hvg['highly_variable_genes'] = [True] * df_hvg.shape[0]
-	df_hvg.set_index('index', inplace = True)
-	df_hvg.drop(columns = ['gene_ids'], inplace = True)
-	adata.var.drop(columns = ['highly_variable_genes'], inplace = True)
-	df_join = adata.var.join(df_hvg)
-	df_join['highly_variable_genes'].fillna(False, inplace = True)
-	adata_hvg = adata[:, df_join['highly_variable_genes']].copy()
+	adata = scc.read_input(in_file)
+	scc.qc_metrics(adata)
+	scc.filter_data(adata)
+	scc.log_norm(adata)
 
-	scCloud.tools.write_output(adata, output_name1)
-	scCloud.tools.write_output(adata_hvg, output_hvg_name1)
+	print("Getting Highly Variable Feature Set...")
+	df_hvf = pd.read_csv(hvf_file)
+	df_hvf['highly_variable_featuress'] = [True] * df_hvf.shape[0]
+	df_hvf.set_index('index', inplace = True)
+	df_hvf.drop(columns = ['gene_ids'], inplace = True)
+	adata.var.drop(columns = ['highly_variable_features'], inplace = True)
+	df_join = adata.var.join(df_hvf)
+	df_join['highly_variable_features'].fillna(False, inplace = True)
+	adata_hvf = adata[:, df_join['highly_variable_features']].copy()
+
+	scc.write_output(adata, output_name1)
+	scc.write_output(adata_hvf, output_hvf_name1)
 
 	# Furthermore, do PCA.
 	suffix2 = "_filter_norm_pca"
-	output_name2 = in_file.split('.')[0] + suffix2
-	output_hvg_name2 = output_name2 + "_hvg"
-	adata_hvg_c = adata_hvg.copy()
-	scCloud.tools.run_pca(adata_hvg_c)
+	output_name2 = os.path.splitext(in_file)[0] + suffix2
+	output_hvf_name2 = output_name2 + "_hvf"
 
-	adata.obsm['X_pca'] = adata_hvg_c.obsm['X_pca']
-	scCloud.tools.write_output(adata, output_name2)
+	scc.pca(adata)
+	scc.write_output(adata, output_name2)
 
-	adata_hvg.obsm['X_pca'] = adata_hvg_c.obsm['X_pca']
-	scCloud.tools.write_output(adata_hvg, output_hvg_name2)
+	scc.pca(adata_hvf)
+	scc.write_output(adata_hvf, output_hvf_name2)
 
 if __name__ == '__main__':
 	extract_hvg()
-	#preprocess_data(full_data_source)
-	#preprocess_data(tiny_data_source)
+	preprocess_data(full_data_source)
+	preprocess_data(tiny_data_source)

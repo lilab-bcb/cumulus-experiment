76d7939e429b2c851e7e862dd0498cc8671dafd0
Yiming Yang <yang.yihming@gmail.com>
2019-09-08 17:33:28 -0400

    fix typo

--- batch_correction/ground/gen_celltype.py
+++ batch_correction/ground/gen_celltype.py
@@ -21,7 +21,7 @@ adata.obs.loc[idx_unknown, 'cell_types'] = res
 adata.obs['cell_types'] = pd.Categorical(adata.obs['cell_types'], categories = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '15', '16', '17', '18', '19'])
 adata.obs['cell_types'] = adata.obs['cell_types'].apply(lambda s: '3' if s == '14' else s)
 adata.obs['cell_types'] = pd.Categorical(adata.obs['cell_types']).rename_categories({'11':'10', '12':'11', '13':'12', '15':'13', '16':'14', '17':'15', '18':'16', '19':'17'})
-scCloud.tools.write_output(adata, "ground_cell_types")
+scc.write_output(adata, "ground_cell_types")
 
 df_celltype = adata.obs[['cell_types']]
 df_celltype.reset_index(inplace = True)

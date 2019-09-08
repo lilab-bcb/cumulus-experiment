298df6b9f36a524287f272d758a94cd9ce1011e4
Yiming Yang <yang.yihming@gmail.com>
2019-09-08 16:57:43 -0400

    Add shell generating ground.h5ad

---
+++ batch_correction/ground/gen_ground.sh
@@ -0,0 +1,3 @@
+sccloud cluster -p 8 --spectral-leiden --umap ../../MantonBM_nonmix_tiny.h5sc ground
+sccloud de_analysis -p 8 --labels spectral_leiden_labels --t ground.h5ad ground.de.xlsx
+sccloud annotate_cluster ground.h5ad ground.anno.txt

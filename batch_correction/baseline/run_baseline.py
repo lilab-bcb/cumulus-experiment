a657227dd12d867b4a6a1a8ac0be837cb1f12822
Yiming Yang <yang.yihming@gmail.com>
2019-09-11 15:42:10 -0400

    use louvain in baseline

--- batch_correction/baseline/run_baseline.py
+++ batch_correction/baseline/run_baseline.py
@@ -3,10 +3,10 @@ import os, sys
 n_jobs = os.cpu_count()
 print("Use {} cores.".format(n_jobs))
 
-if os.system("sccloud cluster -p {cores} --umap ../../MantonBM_nonmix_tiny.h5sc baseline_result".format(cores = n_jobs)):
+if os.system("sccloud cluster -p {cores} --louvain --umap ../../MantonBM_nonmix_tiny.h5sc baseline_result".format(cores = n_jobs)):
 	sys.exit(1)
 
-if os.system("sccloud de_analysis -p 8 --labels spectral_leiden_labels --t baseline_result.h5ad baseline_result.de.xlsx"):
+if os.system("sccloud de_analysis -p 8 --labels louvain_labels --t baseline_result.h5ad baseline_result.de.xlsx"):
 	sys.exit(1)
 
 if os.system("sccloud annotate_cluster baseline_result.h5ad baseline_result.anno.txt"):

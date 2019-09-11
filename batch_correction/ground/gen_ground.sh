53b95c0e0fa52b5f2ba7dc123d25ed4ed96699ab
Yiming Yang <yang.yihming@gmail.com>
2019-09-11 14:44:33 -0400

    Use louvain for batch correction experiment

--- batch_correction/ground/gen_ground.sh
+++ batch_correction/ground/gen_ground.sh
@@ -1,4 +1,4 @@
-sccloud cluster -p 8 --spectral-leiden --umap ../../MantonBM_nonmix_tiny.h5sc ground
-sccloud de_analysis -p 8 --labels spectral_leiden_labels --t ground.h5ad ground.de.xlsx
+sccloud cluster -p 8 --louvain --umap ../../MantonBM_nonmix_tiny.h5sc ground
+sccloud de_analysis -p 8 --labels louvain_labels --t ground.h5ad ground.de.xlsx
 sccloud annotate_cluster ground.h5ad ground.anno.txt
-sccloud plot scatter --basis umap --attributes spectral_leiden_labels ground.h5ad ground.umap.pdf
+sccloud plot scatter --basis umap --attributes louvain_labels ground.h5ad ground.umap.pdf

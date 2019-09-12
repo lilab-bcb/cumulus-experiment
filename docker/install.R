1ef3be5e9b07358345982c003b88d7299064cc58
Yiming Yang <yang.yihming@gmail.com>
2019-09-12 10:22:15 -0400

    Add dependencies for seurat

--- docker/install.R
+++ docker/install.R
@@ -1,5 +1,5 @@
 install.packages('devtools')
-install.packages(c('hdf5r', 'leiden', 'uwot'))
+install.packages(c('hdf5r', 'leiden', 'uwot', 'ape', 'cowplot', 'fitdistrplus', 'future', 'future.apply', 'ggrepel', 'ggridges', 'ica', 'lmtest', 'metap', 'pbapply', 'plotly', 'png', 'RANN', 'ROCR', 'rsvd', 'Rtsne', 'sctransform', 'SDMTools', 'tsne'))
 
 seurat.url <- "https://cran.r-project.org/src/contrib/Seurat_3.1.0.tar.gz"
 install.packages(seurat.url, repos = NULL, type = 'source')

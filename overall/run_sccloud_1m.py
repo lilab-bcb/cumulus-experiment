4a800e0d80de4b7a55c1b128bbeba43dc5144f63
Yiming Yang <yang.yihming@gmail.com>
2019-09-15 13:20:56 -0400

    update 1m sccloud

--- overall/run_sccloud_1m.py
+++ overall/run_sccloud_1m.py
@@ -2,8 +2,8 @@ import os, sys
 
 n_cores = os.cpu_count()
 out_name = "1M_neurons_benchmark"
-data_src = "/projects/benchmark/1M_neurons.h5"
+data_src = "/projects/benchmark/1M_Neurons/1M_neurons.h5"
 
-if os.system("scCloud cluster -p {jobs} --correct-batch-effect --benchmark-time --diffmap-full-speed --run-approximated-leiden --run-fitsne --net-ds-full-speed --run-net-umap --run-net-fle {src_name} {out_name}".format(jobs = n_cores, src_name = data_src, out_name = out_name)):
+if os.system("sccloud cluster -p {jobs} --correct-batch-effect --knn-full-speed --diffmap --spectral-louvain --spectral-leiden --fitsne --net-umap --net-fle {src_name} {out_name} > 1m_sccloud.log".format(jobs = n_cores, src_name = data_src, out_name = out_name)):
 	sys.exit(1)
 

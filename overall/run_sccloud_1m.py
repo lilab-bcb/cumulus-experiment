e6ae8b803bc0fb52c93f1e3b83f5a1699d50e5b4
Yiming Yang <yang.yihming@gmail.com>
2019-09-15 13:42:40 -0400

    set fle memory

--- overall/run_sccloud_1m.py
+++ overall/run_sccloud_1m.py
@@ -4,6 +4,6 @@ n_cores = os.cpu_count()
 out_name = "1M_neurons_benchmark"
 data_src = "/projects/benchmark/1M_Neurons/1M_neurons.h5"
 
-if os.system("sccloud cluster -p {jobs} --correct-batch-effect --knn-full-speed --diffmap --spectral-louvain --spectral-leiden --fitsne --net-umap --net-fle {src_name} {out_name} > 1m_sccloud.log".format(jobs = n_cores, src_name = data_src, out_name = out_name)):
+if os.system("sccloud cluster -p {jobs} --correct-batch-effect --knn-full-speed --diffmap --spectral-louvain --spectral-leiden --fitsne --net-umap --net-fle --fle-memory 128 {src_name} {out_name} > 1m_sccloud.log".format(jobs = n_cores, src_name = data_src, out_name = out_name)):
 	sys.exit(1)
 

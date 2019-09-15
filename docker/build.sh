0bc7ad07a918735e4d3aadf7418def49d3f70578
Yiming Yang <yang.yihming@gmail.com>
2019-09-15 17:27:57 -0400

    Add source code of seurat; change build target name

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 docker build -t sccloud-experiment .
-docker tag sccloud-experiment yihming/sccloud-experiment
-docker push yihming/sccloud-experiment
+docker tag sccloud-experiment sccloud/sccloud-experiment
+docker push sccloud/sccloud-experiment
--- docker/install.R
+++ docker/install.R
@@ -1,5 +1,4 @@
 install.packages('devtools')
-install.packages(c('hdf5r', 'leiden', 'uwot', 'ape', 'cowplot', 'fitdistrplus', 'future', 'future.apply', 'ggrepel', 'ggridges', 'ica', 'lmtest', 'metap', 'pbapply', 'plotly', 'png', 'RANN', 'ROCR', 'rsvd', 'Rtsne', 'sctransform', 'SDMTools', 'tsne'))
+install.packages(c('hdf5r', 'leiden', 'uwot', 'ape', 'cowplot', 'fitdistrplus', 'future', 'future.apply', 'ggrepel', 'ggridges', 'ica', 'lmtest', 'metap', 'pbapply', 'plotly', 'png', 'RANN', 'ROCR', 'rsvd', 'Rtsne', 'sctransform', 'SDMTools', 'tsne', 'RcppAnnoy'))
 
-seurat.url <- "https://cran.r-project.org/src/contrib/Seurat_3.1.0.tar.gz"
-install.packages(seurat.url, repos = NULL, type = 'source')
+devtools::install("/opt/software/seurat-3.1.0")

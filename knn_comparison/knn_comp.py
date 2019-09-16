bf69d4732fe83fb849a535086e099dc2103552b1
Yiming Yang <yang.yihming@gmail.com>
2019-09-15 21:26:18 -0400

    fix bug

--- knn_comparison/knn_comp.py
+++ knn_comparison/knn_comp.py
@@ -156,7 +156,7 @@ def plot_result(df_list):
 
 	# boxplot for kNN recall
 	flierprops = dict(markersize = 0.1)
-	ax = sns.boxplot(x = "method", y = "recall", data = df, order = ["scCloud", "scanpy", "Seurat V3"], linewidth = 0.5, flierprops = flierprops)
+	ax = sns.boxplot(x = "method", y = "recall", data = df, order = ["scCloud", "scanpy", "Seurat Annoy"], linewidth = 0.5, flierprops = flierprops)
 	#ax = sns.stripplot(x = "method", y = "recall", data = df, jitter = True, size = 1)
 	ax.set(ylabel = 'Recall', xlabel = '')
 	vals = ax.get_yticks()
@@ -176,7 +176,7 @@ def plot_result(df_list):
 
 	df_time = df_time.loc[df_time['method'] != 'baseline']
 
-	ax = sns.barplot(x = 'method', y = 'minutes', data = df_time, order = ['scCloud', 'scanpy', 'Seurat V3'])
+	ax = sns.barplot(x = 'method', y = 'minutes', data = df_time, order = ['scCloud', 'scanpy', 'Seurat Annoy'])
 	ax.set(ylabel = 'Total Time in minutes', xlabel = '')
 	ax.get_figure().savefig("knn_time.pdf")
 	cprint("Barplot of total time is generated!", "yellow")

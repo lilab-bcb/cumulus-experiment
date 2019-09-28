04931b188e4b5924186e01fb98c21d65733297c0
Yiming Yang <yang.yihming@gmail.com>
2019-09-28 14:45:18 -0400

    update umap part of 1m scanpy experiment

--- overall/run_scanpy_1m.py
+++ overall/run_scanpy_1m.py
@@ -67,30 +67,30 @@ corrected_data = "/projects/benchmark/1M_Neurons/1M_neurons_corrected.h5ad"
 # Construct affinity matrix using KNN information from scCloud results.
 adata = sc.read_h5ad(corrected_data)
 n_cells = adata.shape[0]
-knn_indices = np.concatenate((np.arange(n_cells).reshape(-1, 1), adata.uns['pca_knn_indices']), axis = 1)
-knn_distances = np.concatenate((np.zeros(n_cells).reshape(-1, 1), adata.uns['pca_knn_distances']), axis = 1)
-distances, connectivities = compute_connectivities_umap(knn_indices, knn_distances, n_obs = n_cells, n_neighbors = 100)
+knn_indices = np.concatenate((np.arange(n_cells).reshape(-1, 1), adata.uns['pca_knn_indices'][:, 0:14]), axis = 1)
+knn_distances = np.concatenate((np.zeros(n_cells).reshape(-1, 1), adata.uns['pca_knn_distances'][:, 0:14]), axis = 1)
+distances, connectivities = compute_connectivities_umap(knn_indices, knn_distances, n_obs = n_cells, n_neighbors = 15)
 adata.uns['neighbors'] = {}
 adata.uns['neighbors']['indices'] = knn_indices
 adata.uns['neighbors']['distances'] = distances
 adata.uns['neighbors']['connectivities'] = connectivities
-adata.uns['neighbors']['params'] = {'n_neighbors': 100, 'method': 'umap', 'metric': 'euclidean'}
+adata.uns['neighbors']['params'] = {'n_neighbors': 15, 'method': 'umap', 'metric': 'euclidean'}
 
 
-print("Finding Clusters using Louvain")
-start_louvain = time.time()
-sc.tl.louvain(adata, resolution = 1.3, random_state = rand_seed)
-end_louvain = time.time()
-logstr_louvain = "Time spent for louvain: {}.".format(timedelta(seconds = end_louvain - start_louvain))
-print(logstr_louvain)
-
-
-print("Finding Clusters using Leiden")
-start_leiden = time.time()
-sc.tl.leiden(adata, resolution = 1.3, random_state = rand_seed)
-end_leiden = time.time()
-logstr_leiden = "Time spent for leiden: {}.".format(timedelta(seconds = end_leiden - start_leiden))
-print(logstr_leiden)
+#print("Finding Clusters using Louvain")
+#start_louvain = time.time()
+#sc.tl.louvain(adata, resolution = 1.3, random_state = rand_seed)
+#end_louvain = time.time()
+#logstr_louvain = "Time spent for louvain: {}.".format(timedelta(seconds = end_louvain - start_louvain))
+#print(logstr_louvain)
+#
+#
+#print("Finding Clusters using Leiden")
+#start_leiden = time.time()
+#sc.tl.leiden(adata, resolution = 1.3, random_state = rand_seed)
+#end_leiden = time.time()
+#logstr_leiden = "Time spent for leiden: {}.".format(timedelta(seconds = end_leiden - start_leiden))
+#print(logstr_leiden)
 
 
 print("Computing UMAP embedding")
@@ -101,28 +101,28 @@ logstr_umap = "Time spent for UMAP: {}.".format(timedelta(seconds = end_umap - s
 print(logstr_umap)
 
 
-print("Computing tSNE embedding")
-start_tsne = time.time()
-sc.tl.tsne(adata, use_rep = 'X_pca', random_state = rand_seed, use_fast_tsne = True)
-end_tsne = time.time()
-logstr_tsne = "Time spent for tSNE: {}.".format(timedelta(seconds = end_tsne - start_tsne))
-print(logstr_tsne)
-
-
-print("Computing diffmap")
-start_df = time.time()
-sc.tl.diffmap(adata, n_comps = 100)
-end_df = time.time()
-logstr_df = "Time spent for diffmap: {}.".format(timedelta(seconds = end_df - start_df))
-print(logstr_df)
-
-adata.write("scanpy_neuron_result_no_fle.h5ad", compression = 'gzip')
-
-print("Computing FLE embedding")
-start_fle = time.time()
-sc.tl.draw_graph(adata, random_state = rand_seed, n_jobs = sc.settings.n_jobs, iterations = 500)
-end_fle = time.time()
-logstr_fle = "Time spent for FLE with {iters} iterations: {time}.".format(iters = 500, time = timedelta(seconds = end_fle - start_fle))
-print(logstr_fle)
-
-adata.write("scanpy_neuron_result.h5ad", compression = 'gzip')
+#print("Computing tSNE embedding")
+#start_tsne = time.time()
+#sc.tl.tsne(adata, use_rep = 'X_pca', random_state = rand_seed, use_fast_tsne = True)
+#end_tsne = time.time()
+#logstr_tsne = "Time spent for tSNE: {}.".format(timedelta(seconds = end_tsne - start_tsne))
+#print(logstr_tsne)
+#
+#
+#print("Computing diffmap")
+#start_df = time.time()
+#sc.tl.diffmap(adata, n_comps = 100)
+#end_df = time.time()
+#logstr_df = "Time spent for diffmap: {}.".format(timedelta(seconds = end_df - start_df))
+#print(logstr_df)
+#
+#adata.write("scanpy_neuron_result_no_fle.h5ad", compression = 'gzip')
+#
+#print("Computing FLE embedding")
+#start_fle = time.time()
+#sc.tl.draw_graph(adata, random_state = rand_seed, n_jobs = sc.settings.n_jobs, iterations = 500)
+#end_fle = time.time()
+#logstr_fle = "Time spent for FLE with {iters} iterations: {time}.".format(iters = 500, time = timedelta(seconds = end_fle - start_fle))
+#print(logstr_fle)
+#
+#adata.write("scanpy_neuron_result.h5ad", compression = 'gzip')

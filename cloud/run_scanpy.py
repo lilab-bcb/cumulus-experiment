8ea8e4d5fef20f11b01adc3bd004dbe8b3b84732
Yiming Yang <yang.yihming@gmail.com>
2019-10-03 16:31:33 -0400

    update scanpy cloud exp

--- cloud/run_scanpy.py
+++ cloud/run_scanpy.py
@@ -5,19 +5,21 @@ import scanpy as sc
 from bbknn import bbknn
 from datetime import timedelta
 
-sc.settings.n_jobs = 64
+sc.settings.n_jobs = 32
 sc.settings.verbosity = 4
 rand_seed = 0
 
-input_file = "/projects/MantonBM/MantonBM_nonmix_10x.h5"
+input_file = "/projects/benchmark/MantonBM/MantonBM_nonmix_10x.h5"
 output_file = "scanpy_analysis_result.h5ad"
 
 print("Reading ICA (bone marrow) dataset")
 adata = sc.read_10x_h5(input_file, genome = 'GRCh38')
 adata.var_names_make_unique()
 
+sc.pp.filter_cells(adata, min_genes = 500)
+sc.pp.filter_cells(adata, max_genes = 6000)
+
 num_cells = adata.shape[0]
-sc.pp.filter_cells(adata, min_counts = 100, min_genes = 500, max_counts = 6e5, max_genes = 6000)
 sc.pp.filter_genes(adata, min_cells = num_cells * 0.0005)
 
 mito_genes = adata.var_names.str.startswith('MT-')
@@ -31,7 +33,7 @@ sc.pp.log1p(adata)
 
 adata.raw = adata.copy()
 
-sc.pp.highly_variable_genes(adata, n_top_genes = 2000)
+sc.pp.highly_variable_genes(adata, n_top_genes = 2000, batch_key = 'Channel')
 adata = adata[:, adata.var['highly_variable']]
 
 sc.pp.scale(adata, max_value = 10)
@@ -43,7 +45,6 @@ sc.tl.louvain(adata, resolution = 1.3, random_state = rand_seed)
 
 sc.tl.tsne(adata, use_rep = 'X_pca', random_state = rand_seed, use_fast_tsne = True)
 
-adata.uns['neighbors']['params']['n_neighbors'] = 15
 sc.tl.umap(adata, random_state = rand_seed)
 
 sc.tl.rank_genes_groups(adata, groupby = 'louvain', method = 't-test')

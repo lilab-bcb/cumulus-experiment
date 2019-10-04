35757fcc4e3ac1ec00aaec9967a70f9f106c7e42
Yiming Yang <yang.yihming@gmail.com>
2019-10-03 21:21:30 -0400

    update seurat exp

--- overall/run_seurat3.R
+++ overall/run_seurat3.R
@@ -1,22 +1,37 @@
 library(Seurat)
 library(R.utils)
 library(parallel)
+library(future)
 
 seed <- 0
 n.cores <- detectCores()
 logfile <- "seurat.log"
 setOption('mc.cores', n.cores)
-print(paste("Use", n.cores, "cores."))
+print(paste("Use", n.cores, "cores for tSNE."))
+
+plan("multiprocess", workers = 28)
+plan()
 
 ## Benchmark Highly Variable Gene selection. ##
 src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
 obj.list <- SplitObject(src.obj, split.by = "Channel")
 print("Finding Highly Variable Genes:")
 now <- Sys.time()
-hvg <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+features <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+logstr.hvgsif <- Sys.time() - now
+print(logstr.hvgsif)
+write(paste("HVG + SIF time:", logstr.hvgsif, attr(logstr.hvgsif, "units")), file = logfile)
+
+src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
+obj.list <- SplitObject(src.obj, split.by = "Channel")
+print("HVG only:")
+now <- Sys.time()
+obj.list <- lapply(X = obj.list, FUN = function(x) {
+	x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000, mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+})
 logstr.hvg <- Sys.time() - now
 print(logstr.hvg)
-write(paste("HVG time:", logstr.hvg, attr(logstr.hvg, "units")), file = logfile)
+write(paste("HVG only time:", logstr.hvg, attr(logstr.hvg, "units")), file = logfile)
 
 ## Benchmark Batch Correction. ##
 ##src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm_hvf.h5ad")
@@ -40,40 +55,42 @@ print(logstr.pca)
 write(paste("PCA time:", logstr.pca, attr(logstr.pca, "units")), file = logfile, append = TRUE)
 
 
-save(adata, file = "seurat_pca.RData")
-
-print("Computing neighborhood graph:")
-n.pc <- dim(adata[["pca"]])[2]
-print(paste0("KNN using ", n.pc, " PCs."))
-write(paste0("KNN using ", n.pc, " PCs"), file = logfile, append = TRUE)
-now <- Sys.time()
-bdata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = FALSE)
-logstr.knn <- Sys.time() - now
-print(logstr.knn)
-write(paste("KNN time:", logstr.knn, attr(logstr.knn, "units")), file = logfile, append = TRUE)
-
-adata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = TRUE)
-save(adata, file = "seurat_knn.RData")
-
-
-print("Finding Clusters:")
-now <- Sys.time()
-adata <- FindClusters(adata, resolution = 1.3, random.seed = seed, algorithm = "louvain")
-logstr.cluster <- Sys.time() - now
-print(logstr.cluster)
-write(paste("Clustering time:", logstr.cluster, attr(logstr.cluster, "units")), file = logfile, append = TRUE)
-
-print("Computing UMAP embedding:")
-now <- Sys.time()
-adata <- RunUMAP(adata, n.neighbors = 15, min.dist = 0.5, seed.use = seed, dims = 1:n.pc)
-logstr.umap <- Sys.time() - now
-print(logstr.umap)
-write(paste("UMAP time:", logstr.umap, attr(logstr.umap, "units")), file = logfile, append = TRUE)
-
-
-print("Computing FItSNE embedding:")
-now <- Sys.time()
-adata <- RunTSNE(adata, seed.use = seed, tsne.method = "FIt-SNE", dims = 1:n.pc)
-logstr.tsne <- Sys.time() - now
-print(logstr.tsne)
-write(paste("TSNE time:", logstr.tsne, attr(logstr.tsne, "units")), file = logfile, append = TRUE)
+##save(adata, file = "seurat_pca.RData")
+##
+##print("Computing neighborhood graph:")
+##n.pc <- dim(adata[["pca"]])[2]
+##print(paste0("KNN using ", n.pc, " PCs."))
+##write(paste0("KNN using ", n.pc, " PCs"), file = logfile, append = TRUE)
+##now <- Sys.time()
+##bdata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = FALSE)
+##logstr.knn <- Sys.time() - now
+##print(logstr.knn)
+##write(paste("KNN time:", logstr.knn, attr(logstr.knn, "units")), file = logfile, append = TRUE)
+##
+##adata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = TRUE)
+##save(adata, file = "seurat_knn.RData")
+##
+##
+##print("Finding Clusters:")
+##now <- Sys.time()
+##adata <- FindClusters(adata, resolution = 1.3, random.seed = seed, algorithm = "louvain")
+##logstr.cluster <- Sys.time() - now
+##print(logstr.cluster)
+##write(paste("Clustering time:", logstr.cluster, attr(logstr.cluster, "units")), file = logfile, append = TRUE)
+##
+##print("Computing UMAP embedding:")
+##now <- Sys.time()
+##adata <- RunUMAP(adata, n.neighbors = 15, min.dist = 0.5, seed.use = seed, dims = 1:n.pc)
+##logstr.umap <- Sys.time() - now
+##print(logstr.umap)
+##write(paste("UMAP time:", logstr.umap, attr(logstr.umap, "units")), file = logfile, append = TRUE)
+##
+##
+##print("Computing FItSNE embedding:")
+##now <- Sys.time()
+##adata <- RunTSNE(adata, seed.use = seed, tsne.method = "FIt-SNE", dims = 1:n.pc)
+##logstr.tsne <- Sys.time() - now
+##print(logstr.tsne)
+##write(paste("TSNE time:", logstr.tsne, attr(logstr.tsne, "units")), file = logfile, append = TRUE)
+##
+##save(adata, file = "seurat_result.RData")

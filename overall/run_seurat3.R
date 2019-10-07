38247222a5ca084d7f6dac808d99fe3eb33062e8
Yiming Yang <yang.yihming@gmail.com>
2019-10-07 12:18:30 -0400

    calculate knn using precomputed pca

--- overall/run_seurat3.R
+++ overall/run_seurat3.R
@@ -9,67 +9,64 @@ logfile <- "seurat.log"
 setOption('mc.cores', n.cores)
 print(paste("Use", n.cores, "cores for tSNE."))
 
-plan("multiprocess", workers = 28)
+plan("multiprocess", workers = n.cores)
 plan()
 
-## Benchmark Highly Variable Gene selection. ##
-src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
-obj.list <- SplitObject(src.obj, split.by = "Channel")
-print("Finding Highly Variable Genes:")
-now <- Sys.time()
-features <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
-logstr.hvgsif <- Sys.time() - now
-write(paste("HVG + SIF time:", logstr.hvgsif, attr(logstr.hvgsif, "units")), file = logfile)
-
-src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
-obj.list <- SplitObject(src.obj, split.by = "Channel")
-print("HVG only:")
-now <- Sys.time()
-obj.list <- lapply(X = obj.list, FUN = function(x) {
-	x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000, mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
-})
-logstr.hvg <- Sys.time() - now
-print(logstr.hvg)
-write(paste("HVG only time:", logstr.hvg, attr(logstr.hvg, "units")), file = logfile, append = TRUE)
-
-## Benchmark Batch Correction. ##
-##src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm_hvf.h5ad")
-##VariableFeatures(src.obj) <- GetAssayData(src.obj)@Dimnames[[1]]
+#### Benchmark Highly Variable Gene selection. ##
+##src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
 ##obj.list <- SplitObject(src.obj, split.by = "Channel")
+##print("Finding Highly Variable Genes:")
 ##now <- Sys.time()
-##bm.anchors <- FindIntegrationAnchors(obj.list)
-##bm.combined <- IntegrateData(bm.anchors)
-##print("Batch Correction:")
-##print(Sys.time() - now)
+##features <- SelectIntegrationFeatures(obj.list, nfeatures = 2000, fvf.nfeatures = 2000, selection.method = "vst", mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+##logstr.hvgsif <- Sys.time() - now
+##write(paste("HVG + SIF time:", logstr.hvgsif, attr(logstr.hvgsif, "units")), file = logfile)
+##
+##src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm.h5ad")
+##obj.list <- SplitObject(src.obj, split.by = "Channel")
+##print("HVG only:")
+##now <- Sys.time()
+##obj.list <- lapply(X = obj.list, FUN = function(x) {
+##	x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000, mean.cutoff = c(0.0125, 7), dispersion.cutoff = c(0.5, Inf))
+##})
+##logstr.hvg <- Sys.time() - now
+##print(logstr.hvg)
+##write(paste("HVG only time:", logstr.hvg, attr(logstr.hvg, "units")), file = logfile, append = TRUE)
+##
+#### Benchmark Batch Correction. ##
+####src.obj <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_filter_norm_hvf.h5ad")
+####VariableFeatures(src.obj) <- GetAssayData(src.obj)@Dimnames[[1]]
+####obj.list <- SplitObject(src.obj, split.by = "Channel")
+####now <- Sys.time()
+####bm.anchors <- FindIntegrationAnchors(obj.list)
+####bm.combined <- IntegrateData(bm.anchors)
+####print("Batch Correction:")
+####print(Sys.time() - now)
+##
+##
+#### Benchmark PCA.
+##adata <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_corrected_hvf.h5ad")
+##print("PCA time:")
+##now <- Sys.time()
+##adata <- ScaleData(adata, scale.max = 10, verbose = FALSE)
+##adata <- RunPCA(adata, verbose = FALSE, seed.use = seed, features = GetAssayData(adata, slot = "data")@Dimnames[[1]])
+##logstr.pca <- Sys.time() - now
+##print(logstr.pca)
+##write(paste("PCA time:", logstr.pca, attr(logstr.pca, "units")), file = logfile, append = TRUE)
+##rm(adata)
 
-
-## Benchmark PCA.
-adata <- ReadH5AD("/projects/benchmark/MantonBM/MantonBM_nonmix_corrected_hvf.h5ad")
-print("PCA time:")
+load("MantonBM_nonmix.RData")
+print("Computing neighborhood graph:")
+n.pc <- dim(adata[["pca"]])[2]
+print(paste0("KNN using ", n.pc, " PCs."))
+write(paste0("KNN using ", n.pc, " PCs"), file = logfile, append = TRUE)
 now <- Sys.time()
-adata <- ScaleData(adata, scale.max = 10, verbose = FALSE)
-adata <- RunPCA(adata, verbose = FALSE, seed.use = seed, features = GetAssayData(adata, slot = "data")@Dimnames[[1]])
-logstr.pca <- Sys.time() - now
-print(logstr.pca)
-write(paste("PCA time:", logstr.pca, attr(logstr.pca, "units")), file = logfile, append = TRUE)
+bdata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy")
+logstr.knn <- Sys.time() - now
+print(logstr.knn)
+write(paste("KNN time:", logstr.knn, attr(logstr.knn, "units")), file = logfile, append = TRUE)
 
+save(adata, file = "seurat_knn.RData")
 
-##save(adata, file = "seurat_pca.RData")
-##
-##print("Computing neighborhood graph:")
-##n.pc <- dim(adata[["pca"]])[2]
-##print(paste0("KNN using ", n.pc, " PCs."))
-##write(paste0("KNN using ", n.pc, " PCs"), file = logfile, append = TRUE)
-##now <- Sys.time()
-##bdata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = FALSE)
-##logstr.knn <- Sys.time() - now
-##print(logstr.knn)
-##write(paste("KNN time:", logstr.knn, attr(logstr.knn, "units")), file = logfile, append = TRUE)
-##
-##adata <- FindNeighbors(adata, k.param = 100, dims = 1:n.pc, nn.method = "annoy", compute.SNN = TRUE)
-##save(adata, file = "seurat_knn.RData")
-##
-##
 ##print("Finding Clusters:")
 ##now <- Sys.time()
 ##adata <- FindClusters(adata, resolution = 1.3, random.seed = seed, algorithm = "louvain")

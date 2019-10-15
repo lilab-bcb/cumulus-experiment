2e6c362a79d8af9c66ca8ff66221055df3f6c1c2
Yiming Yang <yang.yihming@gmail.com>
2019-10-14 22:10:29 -0400

    update pegasus batch correction

--- batch_correction/pegasus/run_sccloud_batch_correct.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-n_jobs = os.cpu_count()
-print("Use {} cores.".format(n_jobs))
-
-if os.system("sccloud cluster -p {cores} --correct-batch-effect --umap /projects/benchmark/MantonBM/MantonBM_nonmix_tiny.h5sc tiny_sccloud_corrected".format(cores = n_jobs)):
-	sys.exit(1)

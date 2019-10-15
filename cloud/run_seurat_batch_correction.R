e4478b6c966c7d389cfb4febad5eaf32f7c876eb
Yiming Yang <yang.yihming@gmail.com>
2019-10-14 22:49:27 -0400

    update

--- cloud/run_seurat_batch_correction.R
+++ cloud/run_seurat_batch_correction.R
@@ -7,7 +7,7 @@ src_file <- "/data/MantonBM_nonmix_10x.h5"
 dst_folder <- "/data/"
 seed <- 0
 debug.mode <- TRUE
-logfile <- paste0(dst_folder, "seurat.log")
+logfile <- paste0(dst_folder, "seurat_hvg_batch_correction.log")
 
 n.cores <- detectCores()
 setOption('mc.cores', n.cores)
@@ -37,7 +37,7 @@ bm.list <- lapply(X = bm.list, FUN = function(x) {
 logstr.hvg <- Sys.time() - now
 write(paste("HVG time:", logstr.hvg, attr(logstr.hvg, "units")), file = logfile, append = TRUE)
 
-plan("multiprocess", workers = 10)
+plan("multiprocess", workers = 2)
 plan()
 write(paste("Use", nbrOfWorkers(), "cores."), file = logfile, append = TRUE)
 
@@ -57,44 +57,3 @@ write(paste("Integration time:", logstr.cca, attr(logstr.cca, "units")), file = 
 DefaultAssay(bm.combined) <- "integrated"
 
 save(bm.combined, file = paste0(dst_folder, "seurat_corrected_result.RData"))
-rm(bm.anchors)
-
-plan("multiprocess", workers = n.cores)
-plan()
-write(paste("Use", nbrOfWorkers(), "cores."), file = logfile, append = TRUE)
-
-now <- Sys.time()
-bm.combined <- ScaleData(bm.combined, scale.max = 10, verbose = debug.mode)
-bm.combined <- RunPCA(bm.combined, npcs = 50, seed.use = seed, verbose = debug.mode)
-logstr.pca <- Sys.time() - now
-write(paste("PCA time:", logstr.pca, attr(logstr.pca, "units")), file = logfile, append = TRUE)
-
-now <- Sys.time()
-bm.combined <- FindNeighbors(bm.combined, reduction = "pca", dims = 1:50, nn.method = 'annoy', k.param = 100, compute.SNN = TRUE, verbose = debug.mode)
-logstr.knn <- Sys.time() - now
-write(paste("KNN time:", logstr.knn, attr(logstr.knn, "units")), file = logfile, append = TRUE)
-
-now <- Sys.time()
-bm.combined <- FindClusters(bm.combined, resolution = 1.3, algorithm = "louvain", random.seed = seed, verbose = debug.mode)
-logstr.louvain <- Sys.time() - now
-write(paste("Louvain time:", logstr.louvain, attr(logstr.louvain, "units")), file = logfile, append = TRUE)
-
-now <- Sys.time()
-bm.combined <- RunTSNE(bm.combined, reduction = "pca", dims = 1:50, tsne.method = 'FIt-SNE', seed.use = seed)
-logstr.tsne <- Sys.time() - now
-write(paste("TSNE time:", logstr.tsne, attr(logstr.tsne, "units")), file = logfile, append = TRUE)
-
-now <- Sys.time()
-bm.combined <- RunUMAP(bm.combined, reduction = "pca", umap.method = "umap-learn", metric = "correlation", n.neighbors = 15, dims = 1:50, seed.use = seed, verbose = debug.mode)
-logstr.umap <- Sys.time() - now
-write(paste("UMAP time:", logstr.umap, attr(logstr.umap, "units")), file = logfile, append = TRUE)
-
-DefaultAssay(bm.combined) <- "RNA"
-save(bm.combined, file = paste0(dst_folder, "seurat_pipeline_result.RData"))
-
-now <- Sys.time()
-bm.markers <- FindAllMarkers(bm.combined, test.use = "t", random.seed = seed, verbose = debug.mode)
-logstr.de <- Sys.time() - now
-write(paste("DE time:", logstr.de, attr(logstr.de, "units")), file = logfile, append = TRUE)
-
-save(bm.markers, file = paste0(dst_folder, "seurat_de_analysis_result.RData"))

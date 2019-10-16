19e965d0caf2813feb8a84044738cb895ab3c57d
Yiming Yang <yang.yihming@gmail.com>
2019-10-16 10:50:57 -0400

    file changes

--- batch_correction/ground/gen_ground.sh
+++
@@ -1,4 +0,0 @@
-sccloud cluster -p 8 --louvain --umap /data/MantonBM_nonmix_tiny.h5sc ground
-sccloud de_analysis -p 8 --labels louvain_labels --t ground.h5ad ground.de.xlsx
-sccloud annotate_cluster ground.h5ad ground.anno.txt
-sccloud plot scatter --basis umap --attributes louvain_labels,Channel ground.h5ad ground.umap.pdf

f839e5013cb31f583486af6837ec65d8d3e9bb2a
Yiming Yang <yang.yihming@gmail.com>
2019-10-17 00:48:44 -0400

    rename sccloud to pegasus

--- batch_correction/pegasus/run_pegasus_batch_correct.py
+++ batch_correction/pegasus/run_pegasus_batch_correct.py
@@ -3,5 +3,5 @@ import os, sys
 n_jobs = os.cpu_count()
 print("Use {} cores.".format(n_jobs))
 
-if os.system("pegasus cluster -p {cores} --correct-batch-effect --umap /data/MantonBM_nonmix_tiny.h5sc tiny_sccloud_corrected > pegasus_correct.log".format(cores = n_jobs)):
+if os.system("pegasus cluster -p {cores} --correct-batch-effect --umap /data/MantonBM_nonmix_tiny.h5sc pegasus_corrected > pegasus_correct.log".format(cores = n_jobs)):
 	sys.exit(1)

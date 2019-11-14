d6aafa9c428cb5c9d07952df454ebc485bd4f389
Yiming Yang <yang.yihming@gmail.com>
2019-11-13 19:39:10 -0500

    fix bug

--- generate_data_info.py
+++ generate_data_info.py
@@ -1,5 +1,5 @@
 import os, sys
-import sccloud as scc
+import pegasus as pg
 import pandas as pd
 from termcolor import cprint
 
@@ -15,12 +15,12 @@ def extract_hvf(input_file):
 
 	# Run correction.
 	cprint("Extracting HVGs from {}...".format(input_file), "green")
-	adata = scc.read_input(input_file)
-	scc.qc_metrics(adata)
-	scc.filter_data(adata)
-	scc.log_norm(adata)
-	scc.highly_variable_features(adata, consider_batch = True)
-	scc.correct_batch(adata, features = "highly_variable_features")
+	adata = pg.read_input(input_file)
+	pg.qc_metrics(adata)
+	pg.filter_data(adata)
+	pg.log_norm(adata)
+	pg.highly_variable_features(adata, consider_batch = True)
+	pg.correct_batch(adata, features = "highly_variable_features")
 
 	# Extract highly variable gene set to file.
 	cprint("Extracting highly variable features to file...", "green")
@@ -28,12 +28,12 @@ def extract_hvf(input_file):
 	df_hvf.to_csv(fname_prefix + "_hvf.txt", index_label = "index")
 
 	# PCA and KNN
-	scc.pca(adata)
-	scc.neighbors(adata)
-	scc.write_output(adata, fname_prefix + "_corrected")
+	pg.pca(adata)
+	pg.neighbors(adata)
+	pg.write_output(adata, fname_prefix + "_corrected")
 
 	bdata = adata[:, adata.var['highly_variable_features']].copy()
-	scc.write_output(bdata, fname_prefix + "_corrected_hvf")
+	pg.write_output(bdata, fname_prefix + "_corrected_hvf")
 
 def preprocess_data(in_file):
 
@@ -44,10 +44,10 @@ def preprocess_data(in_file):
 	output_name1 = os.path.splitext(in_file)[0] + suffix1
 	output_hvf_name1 = output_name1 + "_hvf"
 
-	adata = scc.read_input(in_file)
-	scc.qc_metrics(adata)
-	scc.filter_data(adata)
-	scc.log_norm(adata)
+	adata = pg.read_input(in_file)
+	pg.qc_metrics(adata)
+	pg.filter_data(adata)
+	pg.log_norm(adata)
 
 	print("Getting Highly Variable Feature Set...")
 	df_hvf = pd.read_csv(os.path.splitext(in_file)[0] + "_hvf.txt")
@@ -60,19 +60,19 @@ def preprocess_data(in_file):
 	adata.var['highly_variable_features'] = df_join['highly_variable_features']
 	adata_hvf = adata[:, adata.var['highly_variable_features']].copy()
 
-	scc.write_output(adata, output_name1)
-	scc.write_output(adata_hvf, output_hvf_name1)
+	pg.write_output(adata, output_name1)
+	pg.write_output(adata_hvf, output_hvf_name1)
 
 	# Furthermore, do PCA.
 	suffix2 = "_filter_norm_pca"
 	output_name2 = os.path.splitext(in_file)[0] + suffix2
 	output_hvf_name2 = output_name2 + "_hvf"
 
-	scc.pca(adata)
-	scc.write_output(adata, output_name2)
+	pg.pca(adata)
+	pg.write_output(adata, output_name2)
 
-	scc.pca(adata_hvf)
-	scc.write_output(adata_hvf, output_hvf_name2)
+	pg.pca(adata_hvf)
+	pg.write_output(adata_hvf, output_hvf_name2)
 
 def run_pegasus_process():
 	n_cores = os.cpu_count()

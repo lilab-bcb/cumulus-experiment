aaeb2c4e03a13247382311934953bb274d6de059
Yiming Yang <yang.yihming@gmail.com>
2019-11-24 03:18:37 -0500

    update ground

--- batch_correction/ground/gen_ground_h5ad.py
+++ batch_correction/ground/gen_ground_h5ad.py
@@ -1,9 +1,28 @@
 import os, sys
+import pegasus as pg
 
 n_cores = os.cpu_count()
 
-if os.system("pegasus cluster -p {} --louvain --umap /data/MantonBM_nonmix_tiny.h5sc ground".format(n_cores)):
-	sys.exit(1)
+adata = pg.read_input("/data/MantonBM_nonmix_tiny.h5sc")
+pg.qc_metrics(adata)
+pg.filter_data(adata)
+pg.log_norm(adata)
+pg.highly_variable_features(adata, consider_batch = False)
+
+# Load precalculated PCA
+cprint("Set precalculated PCA info...", "green")
+adata.obsm['X_pca'] = np.load("/data/precalculated/batch_correction/pca.npy")
+adata.uns['PCs'] = np.load("/data/precalculated/batch_correction/PCs.npy")
+adata.uns['pca'] = {}
+adata.uns['pca']['variance'] = np.load("/data/precalculated/batch_correction/pca_variance.npy")
+adata.uns['pca']['variance_ratio'] = np.load("/data/precalculated/batch_correction/pca_variance_ratio.npy")
+
+pg.neighbors(adata)
+pg.louvain(adata)
+pg.umap(adata)
+pg.write_output(adata, "ground")
+
+del adata
 
 if os.system("pegasus de_analysis -p {} --labels louvain_labels --t ground.h5ad ground.de.xlsx".format(n_cores)):
 	sys.exit(1)

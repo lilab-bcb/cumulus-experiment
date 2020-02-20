dd7a7f5fbf2bed5630765526801db1fd4f6eb4b6
Yiming Yang <yang.yihming@gmail.com>
2020-02-20 09:52:37 -0500

    modify docker building

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,1 @@
-docker build -t cumulus-experiment:19.11 .
-docker tag cumulus-experiment:19.11 cumulusprod/cumulus-experiment:19.11
-docker push cumulusprod/cumulus-experiment:19.11
+docker build -t cumulus-experiment:20.02 .
---
+++ docker/upload.sh
@@ -0,0 +1,2 @@
+docker tag cumulus-experiment-20.02 cumulusprod/cumulus-experiment:20.02
+docker push cumulusprod/cumulus-experiment:20.02

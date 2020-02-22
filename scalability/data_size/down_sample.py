a730beb1cc65c839aff32b3855db72d7f1068de2
Yiming Yang <yang.yihming@gmail.com>
2020-02-22 16:05:32 -0500

    rearrange data size experiment

--- scalability/data_size/down_sample.py
+++ scalability/data_size/down_sample.py
@@ -1,11 +1,11 @@
 import os, re, sys
-import pegasus as pg
 import numpy as np
+import pegasus as pg
 
-n_cpu = os.cpu_count()
 data_src = "/data/MantonBM_nonmix.h5sc"
 
 size_list = [5000, 10000, 25000, 50000, 100000, 200000]
+cpu_list = [8, os.cpu_count()]
 
 def down_sample(data, size):
     n_obs = data.shape[0]
@@ -14,25 +14,47 @@ def down_sample(data, size):
     ds_data = data[indices, :].copy()
     return ds_data
 
+def num_to_k(n):
+    num_part = n / 1000
+    if num_part == int(num_part):
+        return str(int(num_part)) + 'k'
+    else:
+        return str(num_part) + 'k'
+
 
 if __name__ == '__main__':
-    f_list = [f for f in os.listdir('.') if re.match('MantonBM_.*_ds.h5ad', f)]
-    if len(f_list) < 6:
+    cmd = sys.argv[1]
+    assert cmd in ['sampling', 'benchmark']
 
+    if cmd == 'sampling':
         adata = pg.read_input(data_src)
         pg.qc_metrics(adata)
         pg.filter_data(adata)
 
         for size in size_list:
-            print("Generating a random sample of size {}:".format(size))
+            size_str = num_to_k(size)
+            print("Generating a random sample of size {}:".format(size_str))
             bdata = down_sample(adata, size)
-            pg.write_output(bdata, "MantonBM_{}_ds.h5ad".format(size))
+            out_name = "MantonBM_{}_ds".format(size_str)
+            pg.write_output(bdata, "{}.h5ad".format(out_name))
 
-    cnt = 1
-    for size in size_list:
-        print("{idx}. Experiment on subsample of MantonBM dataset of size {size}:".format(idx = cnt, size = size))
+    else:
+        cnt = 1
+        for size in size_list:
+            size_str = num_to_k(size)
 
-        if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --diffmap --spectral-louvain --spectral-leiden --fitsne --net-umap --net-fle {src_name} {out_name} > ds_{size}.log".format(jobs = n_cpu, src_name = "MantonBM_{}_ds.h5ad".format(size), out_name = "MantonBM_{}_ds_result.h5ad".format(size), size = size)):
-            sys.exit(1)
+            for cpu in cpu_list:
+                print("{idx}. Experiment on subsample of MantonBM dataset of size {size} with {cpu}:".format(idx = cnt, size = size_str, cpu = cpu))
 
-        cnt += 1
+                if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umap --fle --net-fle {src_name} {out_name} > /output/ds_{size}_cpu_{jobs}.log".format(jobs = cpu, src_name = "MantonBM_{}_ds.h5ad".format(size_str), out_name = "pegasus_{size}_ds_cpu_{cpu}_result.h5ad".format(size = size_str, cpu = cpu), size = size_str)):
+                    sys.exit(1)
+
+                cnt += 1
+
+        for cpu in cpu_list:
+            print("{idx}. Experiment on the full MantonBM dataset with {cpu}:".format(idx = cnt, cpu = cpu))
+
+            if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umnap --fle --net-fle {src_name} {out_name} > /output/ds_fullsize_cpu_{jobs}.log".format(jobs = cpu, src_name = "/data/MantonBM_nonmix.h5sc", out_name = "pegasus_fullsize_cpu_{}_result.h5ad".format(cpu))):
+                sys.exit(1)
+
+            cnt += 1

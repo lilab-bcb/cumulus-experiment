fec328f94e69c11d555c945f65d614bd99d71624
Yiming Yang <yang.yihming@gmail.com>
2020-02-23 20:20:13 -0500

    fix issue in down_sample; add scanpy louvain for bone marrow

--- scalability/down_sample.py
+++ scalability/down_sample.py
@@ -1,6 +1,5 @@
 import os, re, sys
 import numpy as np
-import pegasus as pg
 
 data_src = "/data/MantonBM_nonmix.h5sc"
 
@@ -21,12 +20,16 @@ def num_to_k(n):
     else:
         return str(num_part) + 'k'
 
+def k_to_num(s):
+    return int(float(s[:-1]) * 1000)
+
 
 if __name__ == '__main__':
     cmd = sys.argv[1]
-    assert cmd in ['sampling', 'benchmark']
+    assert cmd in ['sampling', 'benchmark', 'plot']
 
     if cmd == 'sampling':
+        import pegasus as pg
         adata = pg.read_input(data_src)
         pg.qc_metrics(adata)
         pg.filter_data(adata)
@@ -38,7 +41,7 @@ if __name__ == '__main__':
             out_name = "MantonBM_{}_ds".format(size_str)
             pg.write_output(bdata, "{}.h5ad".format(out_name))
 
-    else:
+    elif cmd == 'benchmark':
         cnt = 1
         for size in size_list:
             size_str = num_to_k(size)
@@ -46,7 +49,7 @@ if __name__ == '__main__':
             for cpu in cpu_list:
                 print("{idx}. Experiment on subsample of MantonBM dataset of size {size} with {cpu} cores:".format(idx = cnt, size = size_str, cpu = cpu))
 
-                if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umap --fle --net-fle {src_name} {out_name} > ds_{size}_cpu_{jobs}.log".format(jobs = cpu, src_name = "MantonBM_{}_ds.h5ad".format(size_str), out_name = "pegasus_{size}_ds_cpu_{cpu}_result.h5ad".format(size = size_str, cpu = cpu), size = size_str)):
+                if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umap --fle --net-fle {src_name} {out_name} > ds_{size}_cpu_{jobs}.log".format(jobs = cpu, src_name = "MantonBM_{}_ds.h5ad".format(size_str), out_name = "pegasus_{size}_ds_cpu_{cpu}_result".format(size = size_str, cpu = cpu), size = size_str)):
                     sys.exit(1)
 
                 cnt += 1
@@ -54,7 +57,24 @@ if __name__ == '__main__':
         for cpu in cpu_list:
             print("{idx}. Experiment on the full MantonBM dataset with {cpu} cores:".format(idx = cnt, cpu = cpu))
 
-            if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umap --fle --net-fle {src_name} {out_name} > ds_fullsize_cpu_{jobs}.log".format(jobs = cpu, src_name = "/data/MantonBM_nonmix.h5sc", out_name = "pegasus_fullsize_cpu_{}_result.h5ad".format(cpu))):
+            if os.system("pegasus cluster -p {jobs} --correct-batch-effect --knn-full-speed --louvain --spectral-louvain --leiden --spectral-leiden --fitsne --tsne --umap --net-umap --fle --net-fle {src_name} {out_name} > ds_fullsize_cpu_{jobs}.log".format(jobs = cpu, src_name = "/data/MantonBM_nonmix.h5sc", out_name = "pegasus_fullsize_cpu_{}_result".format(cpu))):
                 sys.exit(1)
 
             cnt += 1
+
+    else:
+        import pandas as pd
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+
+        df = pd.read_csv("stats.csv")
+
+        df['size_num'] = df['size'].apply(lambda s: k_to_num(s))
+        df['Setting'] = df['cpu'].apply(lambda n: 'laptop' if n == 8 else 'server')
+
+        ax = sns.lineplot(x = 'size_num', y = 'tsne', hue = 'Setting', style = 'Setting', markers = True, dashes = False, data = df)
+        ax.set(xlabel = 'Number of cells in dataset', ylabel = 'Time')
+        fig = ax.get_figure()
+        fig.suptitle("Runtime of calculating tSNE embeddings")
+        fig.savefig("test.pdf")
+        plt.close()

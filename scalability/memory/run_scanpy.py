96f9457ff7f76daaaf2a5dd5e36fa6be546f2e45
Yiming Yang <yang.yihming@gmail.com>
2020-02-24 22:09:27 -0500

    restructure scanpy memory test

--- scalability/memory/run_scanpy.py
+++ scalability/memory/run_scanpy.py
@@ -1,25 +1,31 @@
-import os, time
+import os, sys, time
 import numpy as np
 import scanpy as sc
 from datetime import timedelta, datetime
+
+def record_time(start_time, end_time, step_name, fp):
+    fp.write("{step} starts at: {time}\n".format(step = step_name, time = datetime.fromtimestamp(start_time)))
+    fp.write("{step} ends at: {time}\n".format(step = step_name, time = datetime.fromtimestamp(end_time)))
+    fp.write("Time spent for {step}: {time}.\n\n".format(step = step_name, time = timedelta(seconds = end_time - start_time)))
 
 sc.settings.n_jobs = os.cpu_count()
 sc.settings.verbosity = 3
 rand_seed = 0
 
-src_data = "/data/MantonBM_nonmix_10x.h5"
-log_file = "mantonbm_scanpy.log"
+src_data = sys.argv[1]
+out_name = sys.argv[2]
+correct_batch = True if sys.argv[3] == '--batch' else False
+log_file = "{}_scanpy.log".format(out_name)
 
 fp = open(log_file, 'w')
 
 start_read = time.time()
 adata = sc.read_10x_h5(src_data, genome = "GRCh38")
 adata.var_names_make_unique()
-adata.obs['Channel'] = adata.obs.index.map(lambda s: s.split('-')[0]).values
+if correct_batch:
+    adata.obs['Channel'] = adata.obs.index.map(lambda s: s.split('-')[0]).values
 end_read = time.time()
-fp.write("Read starts at: {}\n".format(datetime.fromtimestamp(start_read)))
-fp.write("Read ends at: {}\n".format(datetime.fromtimestamp(end_read)))
-fp.write("Time spent for Read: {}.\n\n".format(timedelta(seconds = end_read - start_read)))
+record_time(start_read, end_read, "Read", fp)
 
 start_filter = time.time()
 n_cells = adata.shape[0]
@@ -28,34 +34,22 @@ sc.pp.filter_cells(adata, max_counts = 600000)
 sc.pp.filter_cells(adata, min_genes = 500)
 sc.pp.filter_cells(adata, max_genes = 6000)
 sc.pp.filter_genes(adata, min_cells = n_cells * 0.0005)
-
 mito_genes = adata.var_names.str.startswith('MT-')
 adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis = 1).A1 / np.sum(adata.X, axis = 1).A1
-adata.obs['n_counts'] = adata.X.sum(axis = 1).A1
 adata = adata[adata.obs.percent_mito < 0.1, :]
 end_filter = time.time()
-
-fp.write("Filter starts at: {}\n".format(datetime.fromtimestamp(start_filter)))
-fp.write("Filter ends at: {}\n".format(datetime.fromtimestamp(end_filter)))
-fp.write("Time spent for Filter: {}.\n\n".format(timedelta(seconds = end_filter - start_filter)))
+record_time(start_filter, end_filter, "Filter", fp)
 
 start_norm = time.time()
 sc.pp.normalize_total(adata, target_sum = 1e5)
 sc.pp.log1p(adata)
 end_norm = time.time()
-fp.write("LogNorm starts at: {}\n".format(datetime.fromtimestamp(start_norm)))
-fp.write("LogNorm ends at: {}\n".format(datetime.fromtimestamp(end_norm)))
-fp.write("Time spent for LogNorm: {}.\n\n".format(timedelta(seconds = end_norm - start_norm)))
-
-#adata.raw = adata
+record_time(start_norm, end_norm, "LogNorm", fp)
 
 start_hvg = time.time()
-sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=7, min_disp=0.5, n_top_genes = 2000, batch_key = 'Channel')
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=7, min_disp=0.5, n_top_genes = 2000, batch_key = 'Channel' if correct_batch else None)
 end_hvg = time.time()
-
-fp.write("HVG starts at: {}\n".format(datetime.fromtimestamp(start_hvg)))
-fp.write("HVG ends at: {}\n".format(datetime.fromtimestamp(end_hvg)))
-fp.write("Time spent for HVG: {}.\n\n".format(timedelta(seconds = end_hvg - start_hvg)))
+record_time(start_hvg, end_hvg, "HVG", fp)
 
 adata = adata[:, adata.var.highly_variable]
 
@@ -63,43 +57,31 @@ start_pca = time.time()
 sc.pp.scale(adata, max_value = 10)
 sc.tl.pca(adata, n_comps = 50, random_state = rand_seed)
 end_pca = time.time()
-fp.write("PCA starts at: {}\n".format(datetime.fromtimestamp(start_pca)))
-fp.write("PCA ends at: {}\n".format(datetime.fromtimestamp(end_pca)))
-fp.write("Time spent for PCA: {}.\n\n".format(timedelta(seconds = end_pca - start_pca)))
+record_time(start_pca, end_pca, "PCA", fp)
 
 start_knn = time.time()
 sc.pp.neighbors(adata, n_neighbors = 100, n_pcs = 50, random_state = 0)
 end_knn = time.time()
-fp.write("KNN starts at: {}\n".format(datetime.fromtimestamp(start_knn)))
-fp.write("KNN ends at: {}\n".format(datetime.fromtimestamp(end_knn)))
-fp.write("Time spent for KNN: {}.\n\n".format(timedelta(seconds = end_knn - start_knn)))
+record_time(start_knn, end_knn, "KNN", fp)
 
 start_louvain = time.time()
 sc.tl.louvain(adata, resolution = 1.3, random_state = rand_seed)
 end_louvain = time.time()
-fp.write("Louvain starts at: {}\n".format(datetime.fromtimestamp(start_louvain)))
-fp.write("Louvain ends at: {}\n".format(datetime.fromtimestamp(end_louvain)))
-fp.write("Time spent for Louvain: {}.\n\n".format(timedelta(seconds = end_louvain - start_louvain)))
+record_time(start_louvain, end_louvain, "Louvain", fp)
 
 start_tsne = time.time()
 sc.tl.tsne(adata, use_rep = 'X_pca', use_fast_tsne = True, random_state = 0)
 end_tsne = time.time()
-fp.write("tSNE starts at: {}\n".format(datetime.fromtimestamp(start_tsne)))
-fp.write("tSNE ends at: {}\n".format(datetime.fromtimestamp(end_tsne)))
-fp.write("Time spent for tSNE: {}.\n\n".format(timedelta(seconds = end_tsne - start_tsne)))
+record_time(start_tsne, end_tsne, "tSNE", fp)
 
 start_umap = time.time()
 sc.tl.umap(adata, random_state = 0)
 end_umap = time.time()
-fp.write("UMAP starts at: {}\n".format(datetime.fromtimestamp(start_umap)))
-fp.write("UMAP ends at: {}\n".format(datetime.fromtimestamp(end_umap)))
-fp.write("Time spent for UMAP: {}.\n\n".format(timedelta(seconds = end_umap - start_umap)))
+record_time(start_umap, end_umap, "UMAP", fp)
 
 start_write = time.time()
 adata.write("mantonbm_scanpy_result.h5ad")
 end_write = time.time()
-fp.write("Write starts at: {}\n".format(datetime.fromtimestamp(start_write)))
-fp.write("Write ends at: {}\n".format(datetime.fromtimestamp(end_write)))
-fp.write("Time spent for Write: {}.\n\n".format(timedelta(seconds = end_write - start_write)))
+record_time(start_write, end_write, "Write", fp)
 
 fp.close()

d19e245e3a419ed0653fce2e3a832c3d8250b8c2
Yiming Yang <yang.yihming@gmail.com>
2020-03-06 16:05:25 -0500

    update more

--- docker/upload.sh
+++ docker/upload.sh
@@ -1,2 +1,2 @@
-docker tag cumulus-experiment-20.03 cumulusprod/cumulus-experiment:20.03
+docker tag cumulus-experiment:20.03 cumulusprod/cumulus-experiment:20.03
 docker push cumulusprod/cumulus-experiment:20.03
